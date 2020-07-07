554ea6053945c500e816f93f76ad1d76b6682db3
Chathura Wanniarachchi <wachathuraj@gmail.com>
2020-07-07 22:41:51 +0530

    Create a copy of draft.php for addResult feature

---
+++ components/exam/addResult.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>University Student-Staff Portal</title>
+    <link rel="stylesheet" href="../../assets/css/main.css">
+</head>
+<body>
+    <!-- header section -->
+    <div class="header">
+        <img src="../../assets/images/UNI_logo.png" class="headerIMG" alt="UNI Logo" height="100px" style="float: left;">
+        <img src="../../assets/images/USSP_logo.png" class="headerIMG" alt="USPS Logo" height="100px" style="float: right;">
+    </div>
+    <!-- log in details section -->
+    <div class="loginDetails">
+        <span class="loginData">Logged as ABS</span>
+    </div>
+    <!-- feature body section -->
+    <div class="featureBody">
+        <!-- develop your content within this div block -->
+        
+    </div>
+    <!-- footer section -->
+    <div class="footer">
+        <span class="footNote">
+        University Student-Staff Portal<br>
+        Developed by <i>Team Binary Bits</i>
+        </span>
+    </div>
+</body>
+</html>
