1ed38d0476f0a015fec6ed3617ad838c3a394857
Chathura Wanniarachchi <wachathuraj@gmail.com>
2020-07-10 01:10:23 +0530

    Update UI with with CSS styles

--- login.php
+++ login.php
@@ -16,18 +16,20 @@
 </head>
 <body>
     <div class="login">
+        <img src="assets/images/UNI_logo.png" alt="UNI Logo"><br>
         <span>University<br> Student-Staff<br> Portal</span><br>
         <form action="" method="post">
-            <div>
-                <input type="text" name="userName" id="userName" class="inputField"><br>
+            <div class="inputPlaceholder">
                 <label for="userName">User Name</label>
+                <input type="text" name="userName" id="userName" class="inputField"><br>
             </div>
-            <div>
-                <input type="password" name="password" id="password" class="inputField"><br>
+            <div class="inputPlaceholder">
                 <label for="password">Password</label>
+                <input type="password" name="password" id="password" class="inputField">
             </div>
-            <input type="submit" value="Log In" name="login" id="login" class="loginButton" style="background-color: green;">
-            <input type="reset" value="Cancel" name="cancel" id="cancel" class="loginButton" style="background-color: red; ">
+            <br>
+                <input type="submit" value="Log In" name="login" id="login" class="loginButton" style="background-color: green;">
+                <input type="reset" value="Cancel" name="cancel" id="cancel" class="loginButton" style="background-color: red; ">
         </form>
     </div>
 </body>
