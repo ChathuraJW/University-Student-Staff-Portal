5e97a7cb516addb7daa9af5d7ad2637ede14aeb5
Chathura Wanniarachchi <wachathuraj@gmail.com>
2020-07-09 16:55:27 +0530

    Remove header and footer section and put into two php files

---
+++ assets/php/commonFooter.php
@@ -0,0 +1,12 @@
+<?php
+    echo("
+    
+    <div class='footer'>
+        <span class='footNote'>
+        University Student-Staff Portal<br>
+        Developed by <i>Team Binary Bits</i>
+        </span>
+    </div>
+    
+    ");
+?>
--- components/exam/addResult.php
+++ components/exam/addResult.php
@@ -8,15 +8,9 @@
     <link rel="stylesheet" href="resultSection.css">
 </head>
 <body>
-    <!-- header section -->
-    <div class="header">
-        <img src="../../assets/images/UNI_logo.png" class="headerIMG" alt="UNI Logo" height="100px" style="float: left;">
-        <img src="../../assets/images/USSP_logo.png" class="headerIMG" alt="USPS Logo" height="100px" style="float: right;">
-    </div>
-    <!-- log in details section -->
-    <div class="loginDetails">
-        <span class="loginData">Logged as ABS</span>
-    </div>
+    <!-- include header section -->
+    <?php require('../../assets/php/commonHeader.php')?>
+    
     <!-- feature body section -->
     <div class="featureBody">
         <h1>Upload Board Confirmed Exam Result</h1>
@@ -58,12 +52,8 @@
             <input type="reset" value="Cancel" name="cancel" id="cancel">
         </form>
     </div>
-    <!-- footer section -->
-    <div class="footer">
-        <span class="footNote">
-        University Student-Staff Portal<br>
-        Developed by <i>Team Binary Bits</i>
-        </span>
-    </div>
+
+    <!-- include footer section -->
+    <?php require('../../assets/php/commonFooter.php')?>
 </body>
 </html>
