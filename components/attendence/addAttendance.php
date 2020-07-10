697e0940fef8b6a59ce0cb8afb24337217d61d53
Shubangi Rathnayake <shubangirathnayake97@gmail.com>
2020-07-10 18:38:57 +0530

    Create a copy of draft.php for add attendance feature

---
+++ components/attendence/addAttendance.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>University Student-Staff Portal</title>
+    <link rel="stylesheet" href="../../assets/css/main.css">
+</head>
+<body>
+    <!-- include header section -->
+    <?php require('../../assets/php/commonHeader.php')?>
+    
+    <!-- feature body section -->
+    <div class="featureBody">
+        
+    </div>
+
+    <!-- include footer section -->
+    <?php require('../../assets/php/commonFooter.php')?>
+</body>
+</html>
