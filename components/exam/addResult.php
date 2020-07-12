3a10f2dc34c0e2ad4d23286cd11fdf279bbc1769
Chathura Wanniarachchi <wachathuraj@gmail.com>
2020-07-12 23:01:52 +0530

    Update form structure and add some functions and styles
    Remove degree program section and add attempt section. Modified submit and reset button sections. Add data filling functions for drop down menus and based on attempt disable batch feature added. Add button styles and modify exist styles. Optimize and clean the code.

--- components/exam/addResult.php
+++ components/exam/addResult.php
@@ -8,101 +8,106 @@
     <link rel="stylesheet" href="resultSection.css">
 </head>
 <body>
-    <!-- include header section -->
-    <?php require('../../assets/php/commonHeader.php')?>
-    
-    <!-- feature body section -->
-    <div class="featureBody">
-        <h1>Upload Board Confirmed Exam Result</h1>
-        <form action="" method="post">
-            <table class="showRadio">
-                <tr>
-                    <td>
-                        <span>Examination for</span>
-                        <div class="radioValue">
-                            <input type="radio" name="examinationName" id="examination1Y" value="1y">
-                            <label for="examination1Y">1<sup>st</sup> Year</label>
-                        </div>
-                        <br>
-                        <div class="radioValue">
-                            <input type="radio" name="examinationName" id="examination2Y" value="2y">
-                            <label for="examination2Y">2<sup>st</sup> Year</label>
-                        </div>
-                        <br>
-                        <div class="radioValue">
-                            <input type="radio" name="examinationName" id="examination3Y" value="3y">
-                            <label for="examination3Y">3<sup>st</sup> Year</label>
-                        </div>
-                        <br>
-                        <div class="radioValue">
-                            <input type="radio" name="examinationName" id="examination4Y" value="4y">
-                            <label for="examination4Y">4<sup>st</sup> Year</label>
-                        </div>
-                    </td>
-                    <td>
-                        <span>Semester</span>
-                        <div class="radioValue">
-                            <input type="radio" name="semester" id="semester1" value="1">
-                            <label for="semester1">1<sup>st</sup> Semester</label>
-                        </div>
-                        <br>
-                        <div class="radioValue">
-                            <input type="radio" name="semester" id="semester2" value="2">
-                            <label for="semester2">2<sup>nd</sup> Semester</label>
-                        </div>
-                        <br>
-                    </td>
-                    <td>
-                        <span>Degree program</span>
-                        <div class="radioValue">
-                            <input type="radio" name="degreeProgram" id="degreeProgramCS" value="cs">
-                            <label for="degreeProgramCS">Computer Science</label>
-                        </div>
-                        <br>
-                        <div class="radioValue">
-                            <input type="radio" name="degreeProgram" id="degreeProgramSE" value="se">
-                            <label for="degreeProgramSE">Software Engineering</label>
-                        </div>
-                        <br>
-                        <div class="radioValue">
-                            <input type="radio" name="degreeProgram" id="degreeProgramIS" value="is">
-                            <label for="degreeProgramIS">Information Systems</label>
-                        </div>
-                    </td>
-                </tr>
-            </table>
-            <br><br>
-            <table class="showRest">
-                <tr>
-                    <td>
-                        <span>Examination Year</span>
-                        <select name="examinationYear" id="examinationYear">
-                        </select>
-                    </td>
-                    <td>
-                        <span>Batch</span>
-                        <select name="batch" id="batch">
-                        </select>
-                    </td>
-                </tr>
-                <tr>
-                    <td>
-                        <span>CSV formatted result dataset</span>
-                        <label for="resultFile" class="fileLabel">Upload Result Data File</label>
-                        <input type="file" name="resultFile" id="resultFile"><br>
-                    </td>
-                    <td>
-                        <input type="submit" value="Submit for Review" name="submit" id="submit">
-                        <input type="reset" value="Cancel" name="cancel" id="cancel">
-                    </td>
-                </tr>
-            </table>
+<!-- include header section -->
+<?php require('../../assets/php/commonHeader.php') ?>
 
-        </form>
-    </div>
+<!-- feature body section -->
+<div class="featureBody">
+    <h1>Upload Board Confirmed Exam Result</h1>
+    <form action="" method="post">
+        <table class="showRadio">
+            <tr>
+                <td>
+                    <span>Examination for</span>
+                    <div class="radioValue">
+                        <input type="radio" name="examinationName" id="examination1Y" value="1y">
+                        <label for="examination1Y">1<sup>st</sup> Year</label>
+                    </div>
+                    <br>
+                    <div class="radioValue">
+                        <input type="radio" name="examinationName" id="examination2Y" value="2y">
+                        <label for="examination2Y">2<sup>st</sup> Year</label>
+                    </div>
+                    <br>
+                    <div class="radioValue">
+                        <input type="radio" name="examinationName" id="examination3Y" value="3y">
+                        <label for="examination3Y">3<sup>st</sup> Year</label>
+                    </div>
+                    <br>
+                    <div class="radioValue">
+                        <input type="radio" name="examinationName" id="examination4Y" value="4y">
+                        <label for="examination4Y">4<sup>st</sup> Year</label>
+                    </div>
+                </td>
+                <td>
+                    <span>Semester</span>
+                    <div class="radioValue">
+                        <input type="radio" name="semester" id="semester1" value="1">
+                        <label for="semester1">1<sup>st</sup> Semester</label>
+                    </div>
+                    <br>
+                    <div class="radioValue">
+                        <input type="radio" name="semester" id="semester2" value="2">
+                        <label for="semester2">2<sup>nd</sup> Semester</label>
+                    </div>
+                    <br>
+                </td>
+                <td>
+                    <span>Attempt</span>
+                    <div class="radioValue">
+                        <input type="radio" name="attempt" id="attemptOne" value="F">
+                        <label for="attemptOne">1<sup>st</sup> Attempt</label>
+                    </div>
+                    <br>
+                    <div class="radioValue">
+                        <input type="radio" name="attempt" id="attemptRep" value="R">
+                        <label for="attemptRep">Repeated Attempt<sup></sup></label>
+                    </div>
+                </td>
+            </tr>
+        </table>
+        <br><br>
+        <table class="showRest">
+            <tr>
+                <td colspan="2">
+                    <span>Subject</span>
+                    <select name="subject" id="subject">
+                    </select>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <span>Examination Year</span>
+                    <select name="examinationYear" id="examinationYear">
+                    </select>
+                </td>
+                <td>
+                    <span>Batch</span>
+                    <select name="batch" id="batch">
+                    </select>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <span>CSV formatted result dataset</span>
+                    <label for="resultFile" class="fileLabel" id="resultFileLabel">Upload Result Data File</label>
+                    <input type="file" name="resultFile" id="resultFile"><br>
+                </td>
+                <td colspan="2">
+                    <br>
+                    <input type="submit" value="Submit for Review" name="submit" id="submit" class="submitCancelButton"
+                           style="background-color: rgb(0,255,0);">
+                    <input type="reset" value="Cancel" name="cancel" id="cancel" class="submitCancelButton"
+                           style="background-color: rgb(255,0,0);">
+                </td>
+            </tr>
+        </table>
 
-    <!-- include footer section -->
-    <?php require('../../assets/php/commonFooter.php')?>
-    <script src="resultSection.js"></script>
+    </form>
+</div>
+
+<!-- include footer section -->
+<?php require('../../assets/php/commonFooter.php') ?>
+<script src="resultSection.js"></script>
 </body>
 </html>
--- components/exam/resultSection.js
+++ components/exam/resultSection.js
@@ -1,19 +1,54 @@
-let currentYear=new Date().getFullYear();
+let currentYear = new Date().getFullYear();
 // fill data to examination year dropdown
-let yearValues=document.getElementById("examinationYear");
-let beginYear=currentYear-10;
-let endYear=currentYear+2;
-while(beginYear<endYear){
-    yearValues.options[yearValues.options.length] = new Option(beginYear.toString(),beginYear.toString());
+let yearValues = document.getElementById("examinationYear");
+let beginYear = currentYear - 10;
+while (beginYear <= currentYear) {
+    yearValues.options[yearValues.options.length] = new Option(beginYear.toString(), beginYear.toString());
     beginYear++;
 }
-yearValues.value=currentYear.toString();
+yearValues.value = currentYear.toString();
+
 // fill data to batch dropdown
-let batchValues=document.getElementById("batch");
+let batchValues = document.getElementById("batch");
 let data;
-beginYear=currentYear-17;
-while (beginYear<currentYear){
-    data=(beginYear-1).toString()+"/"+beginYear.toString();
-    batchValues.options[batchValues.options.length] = new Option(data,data);
+beginYear = currentYear - 17;
+while (beginYear < currentYear) {
+    data = (beginYear - 1).toString() + "/" + beginYear.toString();
+    batchValues.options[batchValues.options.length] = new Option(data, data);
     beginYear++;
 }
+
+//fill data to subject dropdown
+let subjectValues = document.getElementById("subject");
+let subjectName = ["Data Structures & Algorithms 1", "Programming Using C"];
+let subjectCode = ["SCS1201", "SCS1202"];
+let length = subjectCode.length - 1;
+while (length >= 0) {
+    subjectValues.options[subjectValues.options.length] = new Option(subjectName[length], subjectCode[length]);
+    length--;
+}
+
+//uploaded file operation
+let fileUploaded = document.getElementById("resultFile");
+let fileLabel = document.getElementById("resultFileLabel");
+fileUploaded.addEventListener("change", function () {
+    if (fileUploaded.value != '') {
+        let uploadFormat = fileUploaded.value.toString().split('.')[1].toLowerCase();
+        if (uploadFormat == "csv") {
+            fileLabel.style.backgroundColor = "green";
+        } else {
+            fileLabel.style.backgroundColor = "red";
+            alert("Invalid file format. Please upload csv formatted file.")
+        }
+    }
+})
+
+//disable batch option for repeat attempts
+let repeatOptionRep = document.getElementById("attemptRep");
+repeatOptionRep.addEventListener("change", function () {
+    batchValues.disabled = true;
+})
+let repeatOptionOne = document.getElementById("attemptOne");
+repeatOptionOne.addEventListener("change", function () {
+    batchValues.disabled = false;
+})
