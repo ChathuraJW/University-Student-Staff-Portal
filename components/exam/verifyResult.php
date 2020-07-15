2d777ea29659a6cc07c67d17bde2c0950e163e81
Chathura Wanniarachchi <wachathuraj@gmail.com>
2020-07-15 16:48:03 +0530

    Create basic page structure and add some styles

--- components/exam/verifyResult.php
+++ components/exam/verifyResult.php
@@ -15,15 +15,74 @@
     <div class="featureBody">
         <h1>Review Uploaded Result</h1>
         <div class="reviewList">
-            <span>Review List</span>
+            <span class="columnHeader">Review List</span>
+            <div class="reviewListEntry">
+                <div clas="entryDataPoint">
+                    <span class="dataPointHead">Examination Year</span>
+                    <span class="dataPointTail">: 2020</span>
+                </div>
+                <div clas="entryDataPoint">
+                    <span class="dataPointHead">Semester</span>
+                    <span class="dataPointTail">: 1</span>
+                </div>
+                <div clas="entryDataPoint">
+                    <span class="dataPointHead">Examination for</span>
+                    <span class="dataPointTail">: 1st Years</span>
+                </div>
+                <div clas="entryDataPoint">
+                    <span class="dataPointHead">Batch</span>
+                    <span class="dataPointTail">: 2017/18</span>
+                </div>
+                <div clas="entryDataPoint">
+                    <span class="dataPointHead">Subject</span>
+                    <span class="dataPointTail">: SCS2201</span>
+                </div>
+                <div clas="entryDataPoint">
+                    <span class="dataPointHead">Attempt</span>
+                    <span class="dataPointTail">: 1 Attempt</span>
+                </div>
+            </div>
+            <div class="reviewListEntry">
+                <div clas="entryDataPoint">
+                    <span class="dataPointHead">Examination Year</span>
+                    <span class="dataPointTail">: 2020</span>
+                </div>
+                <div clas="entryDataPoint">
+                    <span class="dataPointHead">Semester</span>
+                    <span class="dataPointTail">: 1</span>
+                </div>
+                <div clas="entryDataPoint">
+                    <span class="dataPointHead">Examination for</span>
+                    <span class="dataPointTail">: 1st Years</span>
+                </div>
+                <div clas="entryDataPoint">
+                    <span class="dataPointHead">Batch</span>
+                    <span class="dataPointTail">: 2017/18</span>
+                </div>
+                <div clas="entryDataPoint">
+                    <span class="dataPointHead">Subject</span>
+                    <span class="dataPointTail">: SCS2201</span>
+                </div>
+                <div clas="entryDataPoint">
+                    <span class="dataPointHead">Attempt</span>
+                    <span class="dataPointTail">: 1 Attempt</span>
+                </div>
+            </div>
         </div>
         <div class="showFileContent">
-
+            <span class="columnHeader">Result Review of SCS1201</span>
+            <div class="resultInfo">
+                <span class="resultInfoData">Examination Year : 2020</span>
+                <span class="resultInfoData">Semester : 1</span>
+                <span class="resultInfoData">Examination for  : 1st Years</span>
+                <span class="resultInfoData">Batch : 2017/18</span>
+                <span class="resultInfoData">Subject : SCS2201(Data Structures and Algorithms 1)</span>
+                <span class="resultInfoData">Attempt : 1st Attempt</span>
+            </div>
         </div>
     </div>
 
     <!-- include footer section -->
     <?php require('../../assets/php/commonFooter.php')?>
-    <script src="resultSection.js"></script>
 </body>
 </html>
