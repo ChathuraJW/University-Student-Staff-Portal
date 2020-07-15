951741cfc2af6fccf7d418ae13ba68bf50def33a
Chathura Wanniarachchi <wachathuraj@gmail.com>
2020-07-16 00:31:15 +0530

    Add search function to find data on table
    Add JS to searching and DOM styles.

---
+++ components/exam/verifyResult.js
@@ -0,0 +1,34 @@
+// JS function for search data on data table
+function searchOnTable() {
+    let searchKey = document.getElementById("searchKey");
+    let dataTable = document.getElementById("dataList");
+    let entryCount = document.getElementById("entryCount");
+    let tableRow = dataTable.getElementsByTagName("tr");
+    let tableData, txtValue;
+    let visibleCount = tableRow.length - 1;
+    for (let i = 0; i < tableRow.length; i++) {
+        tableData = tableRow[i].getElementsByTagName("td")[1];
+        if (tableData) {
+            txtValue = tableData.textContent || tableData.innerText;
+            if (txtValue.indexOf(searchKey.value) > -1) {
+                tableRow[i].style.display = "";
+                visibleCount++;
+            } else {
+                tableRow[i].style.display = "none";
+                visibleCount--;
+            }
+        }
+    }
+    // display row count
+    let spanValue = (visibleCount / 2).toString() + " / " + (tableRow.length - 1).toString();
+    if ((visibleCount / 2) == (tableRow.length - 1))
+        entryCount.textContent = " ";
+    else
+        entryCount.textContent = spanValue;
+    // change background color of the search box when search result empty
+    if (visibleCount == 0) {
+        searchKey.style.backgroundColor = "rgba(255, 0, 0, 0.18)";
+    } else {
+        searchKey.style.backgroundColor = "";
+    }
+}
--- components/exam/verifyResult.php
+++ components/exam/verifyResult.php
@@ -8,81 +8,134 @@
     <link rel="stylesheet" href="resultSection.css">
 </head>
 <body>
-    <!-- include header section -->
-    <?php require('../../assets/php/commonHeader.php')?>
+<!-- include header section -->
+<?php require('../../assets/php/commonHeader.php') ?>
 
-    <!-- feature body section -->
-    <div class="featureBody">
-        <h1>Review Uploaded Result</h1>
-        <div class="reviewList">
-            <span class="columnHeader">Review List</span>
-            <div class="reviewListEntry">
-                <div clas="entryDataPoint">
-                    <span class="dataPointHead">Examination Year</span>
-                    <span class="dataPointTail">: 2020</span>
-                </div>
-                <div clas="entryDataPoint">
-                    <span class="dataPointHead">Semester</span>
-                    <span class="dataPointTail">: 1</span>
-                </div>
-                <div clas="entryDataPoint">
-                    <span class="dataPointHead">Examination for</span>
-                    <span class="dataPointTail">: 1st Years</span>
-                </div>
-                <div clas="entryDataPoint">
-                    <span class="dataPointHead">Batch</span>
-                    <span class="dataPointTail">: 2017/18</span>
-                </div>
-                <div clas="entryDataPoint">
-                    <span class="dataPointHead">Subject</span>
-                    <span class="dataPointTail">: SCS2201</span>
-                </div>
-                <div clas="entryDataPoint">
-                    <span class="dataPointHead">Attempt</span>
-                    <span class="dataPointTail">: 1 Attempt</span>
-                </div>
+<!-- feature body section -->
+<div class="featureBody">
+    <h1>Review Uploaded Result</h1>
+    <div class="reviewList">
+        <span class="columnHeader">Review List</span>
+        <div class="reviewListEntry">
+            <div clas="entryDataPoint">
+                <span class="dataPointHead">Examination Year</span>
+                <span class="dataPointTail">: 2020</span>
             </div>
-            <div class="reviewListEntry">
-                <div clas="entryDataPoint">
-                    <span class="dataPointHead">Examination Year</span>
-                    <span class="dataPointTail">: 2020</span>
-                </div>
-                <div clas="entryDataPoint">
-                    <span class="dataPointHead">Semester</span>
-                    <span class="dataPointTail">: 1</span>
-                </div>
-                <div clas="entryDataPoint">
-                    <span class="dataPointHead">Examination for</span>
-                    <span class="dataPointTail">: 1st Years</span>
-                </div>
-                <div clas="entryDataPoint">
-                    <span class="dataPointHead">Batch</span>
-                    <span class="dataPointTail">: 2017/18</span>
-                </div>
-                <div clas="entryDataPoint">
-                    <span class="dataPointHead">Subject</span>
-                    <span class="dataPointTail">: SCS2201</span>
-                </div>
-                <div clas="entryDataPoint">
-                    <span class="dataPointHead">Attempt</span>
-                    <span class="dataPointTail">: 1 Attempt</span>
-                </div>
+            <div clas="entryDataPoint">
+                <span class="dataPointHead">Semester</span>
+                <span class="dataPointTail">: 1</span>
+            </div>
+            <div clas="entryDataPoint">
+                <span class="dataPointHead">Examination for</span>
+                <span class="dataPointTail">: 1st Years</span>
+            </div>
+            <div clas="entryDataPoint">
+                <span class="dataPointHead">Batch</span>
+                <span class="dataPointTail">: 2017/18</span>
+            </div>
+            <div clas="entryDataPoint">
+                <span class="dataPointHead">Subject</span>
+                <span class="dataPointTail">: SCS2201</span>
+            </div>
+            <div clas="entryDataPoint">
+                <span class="dataPointHead">Attempt</span>
+                <span class="dataPointTail">: 1 Attempt</span>
             </div>
         </div>
-        <div class="showFileContent">
-            <span class="columnHeader">Result Review of SCS1201</span>
-            <div class="resultInfo">
-                <span class="resultInfoData">Examination Year : 2020</span>
-                <span class="resultInfoData">Semester : 1</span>
-                <span class="resultInfoData">Examination for  : 1st Years</span>
-                <span class="resultInfoData">Batch : 2017/18</span>
-                <span class="resultInfoData">Subject : SCS2201(Data Structures and Algorithms 1)</span>
-                <span class="resultInfoData">Attempt : 1st Attempt</span>
+        <div class="reviewListEntry">
+            <div clas="entryDataPoint">
+                <span class="dataPointHead">Examination Year</span>
+                <span class="dataPointTail">: 2020</span>
+            </div>
+            <div clas="entryDataPoint">
+                <span class="dataPointHead">Semester</span>
+                <span class="dataPointTail">: 1</span>
+            </div>
+            <div clas="entryDataPoint">
+                <span class="dataPointHead">Examination for</span>
+                <span class="dataPointTail">: 1st Years</span>
+            </div>
+            <div clas="entryDataPoint">
+                <span class="dataPointHead">Batch</span>
+                <span class="dataPointTail">: 2017/18</span>
+            </div>
+            <div clas="entryDataPoint">
+                <span class="dataPointHead">Subject</span>
+                <span class="dataPointTail">: SCS2201</span>
+            </div>
+            <div clas="entryDataPoint">
+                <span class="dataPointHead">Attempt</span>
+                <span class="dataPointTail">: 1 Attempt</span>
             </div>
         </div>
     </div>
-
-    <!-- include footer section -->
-    <?php require('../../assets/php/commonFooter.php')?>
+    <div class="showFileContent">
+        <span class="columnHeader">Result Review of SCS1201</span>
+        <br>
+        <div class="resultInfo">
+            <table>
+                <tr>
+                    <td>Examination Year</td>
+                    <td>: 2020</td>
+                </tr>
+                <tr>
+                    <td>Semester</td>
+                    <td>: 1</td>
+                </tr>
+                <tr>
+                    <td>Examination for</td>
+                    <td>: 1st Years</td>
+                </tr>
+                <tr>
+                    <td>Batch</td>
+                    <td>: 2017/18</td>
+                </tr>
+                <tr>
+                    <td>Subject</td>
+                    <td>: SCS2201(Data Structures and Algorithms 1)</td>
+                </tr>
+                <tr>
+                    <td>Attempt</td>
+                    <td>: 1st Attempt</td>
+                </tr>
+            </table>
+            <br>
+            <div class="searchSection">
+                <span id="entryCount"></span>
+                <input type="search" id="searchKey" onkeyup="searchOnTable()" placeholder="Search...">
+            </div>
+            <br><br><br>
+            <div class="showResult">
+                <table border="1" class="resultTable" id="dataList">
+                    <tr>
+                        <th>Serial No</th>
+                        <th>Index Number</th>
+                        <th>Result</th>
+                    </tr>
+                    <?php $i = 1;
+                    while ($i <= 200) {
+                        $eid = $i + 18001000;
+                        echo("<tr><td>$i</td><td>$eid</td><td>A+</td></tr>");
+                        $i++;
+                    } ?>
+                </table>
+            </div>
+            <br><br>
+            <form action="">
+                <div class="actionButton">
+                    <input type="button" value="Confirm Result Validity" name="submit" id="submit"
+                           class="submitCancelButton"
+                           style="background-color: rgb(0,255,0);">
+                    <input type="button" value="Result Inconsistent" name="cancel" id="cancel"
+                           class="submitCancelButton"
+                           style="background-color: rgb(255,0,0);">
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
+<!-- include footer section -->
+<?php require('../../assets/php/commonFooter.php') ?>
+<script src="verifyResult.js"></script>
 </body>
 </html>
