6bfaef5fa178beabddca18bc722a348552557868
Chathura Wanniarachchi <wachathuraj@gmail.com>
2020-07-15 15:00:05 +0530

    Create a copy of draft.php for verifyResult feature

---
+++ components/exam/verifyResult.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>University Student-Staff Portal</title>
+    <link rel="stylesheet" href="../../assets/css/main.css">
+    <link rel="stylesheet" href="resultSection.css">
+</head>
+<body>
+    <!-- include header section -->
+    <?php require('../../assets/php/commonHeader.php')?>
+
+    <!-- feature body section -->
+    <div class="featureBody">
+        <h1>Review Uploaded Result</h1>
+        <div class="reviewList">
+            <span>Review List</span>
+        </div>
+        <div class="showFileContent">
+
+        </div>
+    </div>
+
+    <!-- include footer section -->
+    <?php require('../../assets/php/commonFooter.php')?>
+    <script src="resultSection.js"></script>
+</body>
+</html>
