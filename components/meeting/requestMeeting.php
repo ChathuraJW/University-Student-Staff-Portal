f5a26bc2ebd2ad418bf6c0e20b87d6bfa32ab7d9
Rjitha Chamod <trchamod98@gmail.com>
2020-07-17 19:38:06 +0530

    Create few changes for requestMeeting.php file

--- components/meeting/requestMeeting.php
+++ components/meeting/requestMeeting.php
@@ -13,50 +13,61 @@
     <!-- feature body section -->
     <div class="featureBody">
         <h2>Make Your Request</h2>
-        <!-- fist lecturer and date -->
-        <label for="lecture1">Lecturer </label>
-        <input list="lecture1">
-        <datalist id="lecture1">
-            <option value="LEC1045"></option>
-            <option value="LEC1305"></option>
-            <option value="LEC1023"></option>
-            <option value="LEC1245"></option>
-            <option value="LEC1395"></option>
-            <option value="LEC1294"></option>
-        </datalist> 
-        <label for="date1">Date </label>
-        <input type="date" id="date1"><br>
-
-        <!-- object for lecturer shedule of the day -->
-        <object data=""></object><br>
-        <form method="get" action="submit.php">
         <fieldset>
-        <label for="lecture2">Lecturer </label>
-        <input list="lecture2">
-        <datalist id="lecture2">
-            <option value="LEC1045"></option>
-            <option value="LEC1305"></option>
-            <option value="LEC1023"></option>
-            <option value="LEC1245"></option>
-            <option value="LEC1395"></option>
-            <option value="LEC1294"></option>
-        </datalist> <br>
-        <label for="date2">Date </label>
-        <input type="date" id="date2"><br>
+        <h4>Lecturer Availability</h4>
+            <label for="lecture1">Lecturer </label>
+            <input list="lecture1">
+            <datalist id="lecture1">
+                <option value="LEC1045"></option>
+                <option value="LEC1305"></option>
+                <option value="LEC1023"></option>
+                <option value="LEC1245"></option>
+                <option value="LEC1395"></option>
+                <option value="LEC1294"></option>
+            </datalist> <br>
+            
+            <button type="submit" value="Submit">check</button><br>
 
-        <label for="time">Time </label>
-        <input type="time" id="time"><br>
+            <!-- Display the availability of the lecturer -->
+            
+                <p>Lecturer is</p><br>
 
-        <label for="description">Description </label>
-        <input type="text" id="description"><br>
-        
-        <button type="submit" value="Submit">Request an Appointment</button>
-        
-        
-        <button type="reset">Reset</button>
-        </form>
-        <!-- <input type="submit" action="/submit" value="Request an Appointment"> -->
         </fieldset>
+        <!-- fist lecturer and date -->
+        
+
+        <form method="get" action="submit.php">
+
+            <fieldset>
+                <label for="lecture2">Lecturer </label>
+                <input list="lecture2">
+                <datalist id="lecture2">
+                    <option value="LEC1045"></option>
+                    <option value="LEC1305"></option>
+                    <option value="LEC1023"></option>
+                    <option value="LEC1245"></option>
+                    <option value="LEC1395"></option>
+                    <option value="LEC1294"></option>
+                </datalist> <br>
+                <label for="date2">Date </label>
+                <input type="date" id="date2"><br>
+
+                <label for="time">Time </label>
+                <input type="time" id="time"><br>
+
+                <label for="description">Description </label>
+                <input type="text" id="description"><br>
+                
+                <button type="submit" value="Submit">Request an Appointment</button>
+                
+                
+                <button type="reset">Reset</button>
+
+            </fieldset>
+
+        </form>
+        
+        
     </div>
     
 
