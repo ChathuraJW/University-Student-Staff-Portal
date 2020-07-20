3a93a13c76f0258e4762f6cae5ba0dd6dd10a489
Rjitha Chamod <trchamod98@gmail.com>
2020-07-20 16:01:21 +0530

    Make some changes on requestMeeting.php

--- components/meeting/requestMeeting.php
+++ components/meeting/requestMeeting.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>University Student-Staff Portal</title>
     <link rel="stylesheet" href="../../assets/css/main.css">
+    <link rel="stylesheet" href="../../assets/css/requestMeetingStyles.css">
 </head>
 <body>
     <!-- include header section -->
@@ -12,9 +13,9 @@
     
     <!-- feature body section -->
     <div class="featureBody">
-        <h2>Make Your Request</h2>
-        <fieldset>
-        <h4>Lecturer Availability</h4>
+        <h2 class="h1">Chek Availability</h2>
+        <form class="form1">
+        <h4 class="h2">Lecturer Availability</h4>
             <label for="lecture1">Lecturer </label>
             <input list="lecture1">
             <datalist id="lecture1">
@@ -26,21 +27,24 @@
                 <option value="LEC1294"></option>
             </datalist> <br>
             
-            <button type="submit" value="Submit">check</button><br>
+            <button class="buttoncheck"  type="submit" value="Submit">check</button><br>
 
             <!-- Display the availability of the lecturer -->
             
-                <p>Lecturer is</p><br>
+                <p>Lecturer is   </p>
+                
 
-        </fieldset>
+        </form>
         <!-- fist lecturer and date -->
         
 
-        <form method="get" action="submit.php">
+        <div class="requestform">
+            <h2 class="newhead">New Appointment</h2>
+            <form class="form2" method="get" action="submit.php">
 
-            <fieldset>
-                <label for="lecture2">Lecturer </label>
-                <input list="lecture2">
+                        
+                <label class="lable" for="lecture2">Lecturer </label>
+                <input class="dataraws" list="lecture2">
                 <datalist id="lecture2">
                     <option value="LEC1045"></option>
                     <option value="LEC1305"></option>
@@ -49,23 +53,24 @@
                     <option value="LEC1395"></option>
                     <option value="LEC1294"></option>
                 </datalist> <br>
-                <label for="date2">Date </label>
-                <input type="date" id="date2"><br>
+                <label class="lable" for="date2">Date </label>
+                <input class="dataraws" type="date" id="date2"><br>
 
-                <label for="time">Time </label>
-                <input type="time" id="time"><br>
+                <label class="lable" for="time">Time </label>
+                <input class="dataraws" type="time" id="time"><br>
 
-                <label for="description">Description </label>
-                <input type="text" id="description"><br>
-                
-                <button type="submit" value="Submit">Request an Appointment</button>
-                
-                
-                <button type="reset">Reset</button>
+                <label class="lable" for="description">Description </label>
+                <input class="discription1" type="text" id="description"><br>
 
-            </fieldset>
+                <button class="button" totype="submit" value="Submit">Request an Appointment</button>
 
-        </form>
+
+                <button class="button" type="reset">Reset</button>
+
+
+
+            </form>
+        </div>
         
         
     </div>
