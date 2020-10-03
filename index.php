5cedb1d22135444f620a45847130f7f32044c9a4
Chathura Wanniarachchi <wachathuraj@gmail.com>
2020-10-03 12:15:22 +0530

    Fixed style loading error and apply fontawesome for styling
    fontawesome library is used to add log out icon. As well as do changes to color combinations of headers and apply simple footer.

--- index.php
+++ index.php
@@ -4,26 +4,36 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>University Student-Staff Portal</title>
-    <link rel="stylesheet" href="/assets/css/mainStyle.css">
+    <link rel="stylesheet" href="assets/css/main.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.0/css/all.min.css" />
 </head>
 <body>
     <!-- header section -->
     <header>
         <div class="overlay">
             <div class="imgSection">
-                <img src="assets/images/UCSC_logo.png" alt="UCSC_logo" />
+                <img src="assets/images/UOC_logo.png" alt="UOC_logo" />
             </div>
             <div class="textSection">
                 <span class="mainText">University Student-Staff Portal</span>
             </div>
             <div class="imgSection">
-                <img src="assets/images/UOC_logo.png" alt="UOC_logo" />
+                <img src="assets/images/UCSC_logo.png" alt="UCSC_logo" />
             </div>
             <h3>University of Colombo School of Computing</h3>
         </div>
     </header>
-    <div class="login">
-        <h4 class="loginInfo">Login as Chathura &nbsp;&nbsp;&nbsp;<span><a href="#" style="font-size: 80%;color: white;">Log Out..</a></span> </h4>
+    <div class="loginInfo">
+        <h4>Login as Chathura &nbsp;<span ><a href="#" style="color: white;"><i class="fas fa-sign-out-alt" style="color: white;"></i></a></span></h4>
     </div>
+
+    <div class="content">
+
+    </div>
+
+<footer class="mainFooter">
+    <br>
+    &copy; developed by Team Binary Bits
+</footer>
 </body>
 </html>
