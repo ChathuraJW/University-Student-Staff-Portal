25d02fe0e6b3322d9f72a03f8309cc01bb6150f2
Chathura Wanniarachchi <wachathuraj@gmail.com>
2020-10-04 11:05:40 +0530

    notification section styling completed but responsiveness is not yet.
    Notification coloring , and profile details are styled.  Color coding styles is on style file.

--- index.php
+++ index.php
@@ -5,75 +5,121 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>University Student-Staff Portal</title>
     <link rel="stylesheet" href="assets/css/main.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.0/css/all.min.css" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.0/css/all.min.css"/>
 </head>
 <body>
-    <!-- header section -->
-    <header>
-        <div class="overlay">
-            <div class="imgSection">
-                <img src="assets/images/UOC_logo.png" alt="UOC_logo" />
-            </div>
-            <div class="textSection">
-                <span class="mainText">University Student-Staff Portal</span>
-            </div>
-            <div class="imgSection">
-                <img src="assets/images/UCSC_logo.png" alt="UCSC_logo" />
-            </div>
-            <h3>University of Colombo School of Computing</h3>
+<!-- header section -->
+<header>
+    <div class="overlay">
+        <div class="imgSection">
+            <img src="assets/images/UOC_logo.png" alt="UOC_logo"/>
         </div>
-    </header>
-    <div class="loginInfo">
-        <h4>Login as Chathura &nbsp;<span ><a href="#" style="color: white;"><i class="fas fa-sign-out-alt" style="color: white;"></i></a></span></h4>
+        <div class="textSection">
+            <span class="mainText">University Student-Staff Portal</span>
+        </div>
+        <div class="imgSection">
+            <img src="assets/images/UCSC_logo.png" alt="UCSC_logo"/>
+        </div>
+        <h3>University of Colombo School of Computing<br>Sri Lanka</h3>
     </div>
+</header>
+<div class="loginInfo">
+    <h4>Login as Chathura &nbsp;<span><a href="#" style="color: white;"><i class="fas fa-sign-out-alt"
+                                                                           style="color: white;"></i></a></span></h4>
+</div>
 
-    <div class="content">
-        <div class="topSection">
-            <div class="notificationBlock">
-                <div class="notificationStack">
-                    <div class="stackHeader">
+<div class="content">
+    <div class="topSection">
+        <div class="notificationBlock">
+            <div class="notificationStack">
+                <div class="stackHeader">
                     <span class="stackLabel"><i class="fas fa-bullhorn"></i>&nbsp;&nbsp;Academic Timetable</span>
-                        <span class="notificationCount">45</span>
-                    </div>
-                    <div class="notificationEntry"></div>
-                    <div class="notificationEntry"></div>
-                    <div class="notificationEntry"></div>
-                    <div class="notificationEntry"></div>
                 </div>
-                <div class="notificationStack">
-                    <div class="stackHeader">
-                        <span class="stackLabel"><i class="fas fa-bullhorn"></i>&nbsp;&nbsp;Academic Notification</span>
-                        <span class="notificationCount">57</span>
-                    </div>
-                    <div class="notificationEntry"></div>
-                    <div class="notificationEntry"></div>
+                <div class="notificationEntryTimeTable">
+                    <i class="fas fa-chalkboard-teacher"></i>&nbsp;&nbsp;SCS2201 Lecture<br>
+                    <i class="fas fa-map-marked-alt"></i>&nbsp;&nbsp;S203 <br>
+                    <i class="far fa-clock"></i>&nbsp; 08:00 - 10:00 <br>
                 </div>
-                <div class="notificationStack">
-                    <div class="stackHeader">
-                    <span class="stackLabel"><i class="fas fa-bullhorn"></i>&nbsp;&nbsp;General Notification</span>
-                        <span class="notificationCount">67</span>
-                    </div>
-                    <div class="notificationEntry"></div>
-                    <div class="notificationEntry"></div>
+                <div class="notificationEntryTimeTable">
+                    <i class="fas fa-chalkboard-teacher"></i>&nbsp;&nbsp;SCS2204 Practical<br>
+                    <i class="fas fa-map-marked-alt"></i>&nbsp;&nbsp;LabA LabB LabC<br>
+                    <i class="far fa-clock"></i>&nbsp; 10:00 - 12:00 <br>
                 </div>
-                <div class="notificationStack">
-                    <div class="stackHeader">
-                    <span class="stackLabel"><i class="fas fa-bullhorn"></i>&nbsp;&nbsp;System Notification</span>
-                        <span class="notificationCount">46</span>
-                    </div>
-                    <div class="notificationEntry"></div>
-                    <div class="notificationEntry"></div>
+                <div class="notificationEntryTimeTable">
+                    <i class="fas fa-chalkboard-teacher"></i>&nbsp;&nbsp;S2205 Tutorial<br>
+                    <i class="fas fa-map-marked-alt"></i>&nbsp;&nbsp;E401 <br>
+                    <i class="far fa-clock"></i>&nbsp; 13:00 - 15:00 <br>
+                </div>
+                <div class="notificationEntryTimeTable">
+                    <i class="fas fa-chalkboard-teacher"></i>&nbsp;&nbsp;SCS2206 Lecture<br>
+                    <i class="fas fa-map-marked-alt"></i>&nbsp;&nbsp;S204 <br>
+                    <i class="far fa-clock"></i>&nbsp; 15:00 - 17:00 <br>
                 </div>
             </div>
-            <div class="profileSection">
-                <div class="profilePic"></div>
-                <div class="userInfo"></div>
+            <div class="notificationStack">
+                <div class="stackHeader">
+                    <span class="stackLabel"><i class="fas fa-bullhorn"></i>&nbsp;&nbsp;Academic Notification</span>
+                    <span class="notificationCount">57</span>
+                </div>
+                <div class="notificationEntry blue">
+                    <div class="notificationIcon"><i class="fas fa-school"></i></div>
+                    <div class="notificationContent">SCS2203 In class assignment 1 will be on 22th november.</div>
+                </div>
+                <div class="notificationEntry green">
+                    <div class="notificationIcon"><i class="fas fa-school"></i></div>
+                    <div class="notificationContent">2nd semester exam will commence on 6th September.</div>
+                </div>
+            </div>
+            <div class="notificationStack">
+                <div class="stackHeader">
+                    <span class="stackLabel"><i class="fas fa-bullhorn"></i>&nbsp;&nbsp;General Notification</span>
+                    <span class="notificationCount">67</span>
+                </div>
+                <div class="notificationEntry blue">
+                    <div class="notificationIcon"><i class="fas fa-scroll"></i></div>
+                    <div class="notificationContent">Calling application for Mahapola scholarship</div>
+                </div>
+                <div class="notificationEntry red">
+                    <div class="notificationIcon red"><i class="fas fa-scroll"></i></div>
+                    <div class="notificationContent">Academic activities for 2nd semester will commence on 19th October
+                        2020.
+                    </div>
+                </div>
+                <div class="notificationEntry gray">
+                    <div class="notificationIcon"><i class="fas fa-scroll"></i></div>
+                    <div class="notificationContent">Welfare society of staff will plan to have a fund racing
+                        activity.
+                    </div>
+                </div>
+            </div>
+            <div class="notificationStack">
+                <div class="stackHeader">
+                    <span class="stackLabel"><i class="fas fa-bullhorn"></i>&nbsp;&nbsp;System Notification</span>
+                    <span class="notificationCount">46</span>
+                </div>
+                <div class="notificationEntry orange">
+                    <div class="notificationIcon"><i class="fas fa-laptop-house"></i></i></div>
+                    <div class="notificationContent">System will shutdown for next 5 hours for maintenance</div>
+                </div>
             </div>
         </div>
-        <div class="linkSection">
+        <div class="profileSection">
+            <div class="profilePic">
+                <img src="assets/images/userMale.jpg" alt="profilePic" style="height: auto;width: 100%;margin: auto">
+            </div>
+            <div class="userInfo">
+                <span class="name">W.A.C.J.<br>Wanniarachchci</span>
+                <span class="emailPersonal">wchaturaj@gmail.com</span>
+                <span class="emailUniversity">2018cs183@ucsc.cmb.ac.lk</span><br>
+                <span class="gpa green">3.653</span>
 
+            </div>
         </div>
     </div>
+    <div class="linkSection">
+
+    </div>
+</div>
 
 <footer class="mainFooter">
     <br>
