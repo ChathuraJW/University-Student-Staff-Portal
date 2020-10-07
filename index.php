02ee1118f13a5de58911fa8ff2476f0d3e7a7312
Chathura Wanniarachchi <wachathuraj@gmail.com>
2020-10-07 08:28:23 +0530

    Apply grid system for responsiveness🎉
    Home made grid system apply for the project to maintain responsiveness of the site.

--- index.php
+++ index.php
@@ -5,35 +5,35 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>University Student-Staff Portal</title>
     <link rel="stylesheet" href="assets/css/main.css">
+    <link rel="stylesheet" href="assets/css/gridSystem.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.0/css/all.min.css"/>
 </head>
 <body>
 <!-- header section -->
 <header>
     <div class="overlay">
-        <div class="imgSection">
-            <img src="assets/images/UOC_logo.png" alt="UOC_logo"/>
-        </div>
-        <div class="textSection">
-            <span class="mainText">University Student-Staff Portal</span>
-        </div>
-        <div class="imgSection">
-            <img src="assets/images/UCSC_logo.png" alt="UCSC_logo"/>
-        </div>
-        <h3>University of Colombo School of Computing<br>Sri Lanka</h3>
+            <div class="imgSection">
+                <img src="assets/image/UOC_logo.png" alt="UOC_logo"/>
+            </div>
+            <div class="textSection">
+                <span class="mainText">University Student-Staff Portal</span>
+            </div>
+            <div class="imgSection">
+                <img src="assets/image/UCSC_logo.png" alt="UCSC_logo"/>
+            </div>
+        <h3 class="uniName">University of Colombo School of Computing<br>Sri Lanka</h3>
     </div>
 </header>
 <div class="loginInfo">
-    <h4>Login as Chathura &nbsp;<span><a href="#" style="color: white;"><i class="fas fa-sign-out-alt"
-                                                                           style="color: white;"></i></a></span></h4>
+    <h4>Login as Chathura &nbsp;<span><a href="#" style="color: white;"><i class="fas fa-sign-out-alt" style="color: white;"></i></a></span></h4>
 </div>
 
 <div class="content">
     <div class="topSection">
-        <div class="notificationBlock">
+        <div class="row col-5">
             <div class="notificationStack">
                 <div class="stackHeader">
-                    <span class="stackLabel"><i class="fas fa-bullhorn"></i>&nbsp;&nbsp;Academic Timetable</span>
+                    <span class="stackLabel">Academic Schedule</span>
                 </div>
                 <div class="notificationEntryTimeTable">
                     <i class="fas fa-chalkboard-teacher"></i>&nbsp;&nbsp;SCS2201 Lecture<br>
@@ -58,7 +58,7 @@
             </div>
             <div class="notificationStack">
                 <div class="stackHeader">
-                    <span class="stackLabel"><i class="fas fa-bullhorn"></i>&nbsp;&nbsp;Academic Notification</span>
+                    <span class="stackLabel">Academic Notification</span>
                     <span class="notificationCount">57</span>
                 </div>
                 <div class="notificationEntry blue">
@@ -70,9 +70,22 @@
                     <div class="notificationContent">2nd semester exam will commence on 6th September.</div>
                 </div>
             </div>
+            <div class="profileSection">
+                <a href="" class="userSetting"><i class="fas fa-cog fa-2x"></i></a>
+                <div class="profilePic">
+                    <img src="assets/image/userMale.jpg" alt="profilePic" style="height: auto;width: 100%;margin: auto">
+                </div>
+                <div class="userInfo">
+                    <span class="name">W.A.C.J.<br>Wanniarachchci</span><br>
+                    <span class="emailPersonal">wchaturaj@gmail.com</span><br>
+                    <span class="emailUniversity">2018cs183@ucsc.cmb.ac.lk</span><br>
+                    <span class="gpa green">3.653</span>
+
+                </div>
+            </div>
             <div class="notificationStack">
                 <div class="stackHeader">
-                    <span class="stackLabel"><i class="fas fa-bullhorn"></i>&nbsp;&nbsp;General Notification</span>
+                    <span class="stackLabel">General Notification</span>
                     <span class="notificationCount">67</span>
                 </div>
                 <div class="notificationEntry blue">
@@ -94,7 +107,7 @@
             </div>
             <div class="notificationStack">
                 <div class="stackHeader">
-                    <span class="stackLabel"><i class="fas fa-bullhorn"></i>&nbsp;&nbsp;System Notification</span>
+                    <span class="stackLabel">System Notification</span>
                     <span class="notificationCount">46</span>
                 </div>
                 <div class="notificationEntry orange">
@@ -102,22 +115,115 @@
                     <div class="notificationContent">System will shutdown for next 5 hours for maintenance</div>
                 </div>
             </div>
-        </div>
-        <div class="profileSection">
-            <div class="profilePic">
-                <img src="assets/images/userMale.jpg" alt="profilePic" style="height: auto;width: 100%;margin: auto">
-            </div>
-            <div class="userInfo">
-                <span class="name">W.A.C.J.<br>Wanniarachchci</span>
-                <span class="emailPersonal">wchaturaj@gmail.com</span>
-                <span class="emailUniversity">2018cs183@ucsc.cmb.ac.lk</span><br>
-                <span class="gpa green">3.653</span>
-
-            </div>
-        </div>
+    </div>
     </div>
     <div class="linkSection">
-
+        <div class="row col-6">
+            <a href="#" class="tile">
+                <div class="tileImage"><i class="fas fa-laptop-code fa-3x"></i></div>
+                <div class="tileDescription">Access to VLE</div>
+            </a>
+            <a href="#" class="tile">
+                <div class="tileImage"><i class="fas fa-poll fa-3x"></i></div>
+                <div class="tileDescription">Student Result</div>
+            </a>
+            <a href="#" class="tile">
+                <div class="tileImage"><i class="fas fa-file-signature fa-3x"></i></div>
+                <div class="tileDescription">Student Attendance</div>
+            </a>
+            <a href="#" class="tile">
+                <div class="tileImage"><i class="fas fa-calendar-check fa-3x"></i></div>
+                <div class="tileDescription">Appointment for Meeting</div>
+            </a>
+            <a href="#" class="tile">
+                <div class="tileImage"><i class="fas fa-university fa-3x"></i></div>
+                <div class="tileDescription">Hall Booking</div>
+            </a>
+            <a href="#" class="tile">
+                <div class="tileImage"><i class="fas fa-folder-open fa-3x"></i></div>
+                <div class="tileDescription">Personal Files</div>
+            </a>
+            <a href="#" class="tile">
+                <div class="tileImage"><i class="fas fa-headset fa-3x"></i></div>
+                <div class="tileDescription">Contact Union</div>
+            </a>
+            <a href="#" class="tile">
+                <div class="tileImage"><i class="fas fa-train fa-3x"></i></div>
+                <div class="tileDescription">Train Season</div>
+            </a>
+            <a href="#" class="tile">
+                <div class="tileImage"><i class="fas fa-file-pdf fa-3x"></i></div>
+                <div class="tileDescription">Past Paper</div>
+            </a>
+            <a href="#" class="tile">
+                <div class="tileImage"><i class="fas fa-comment-dots fa-3x"></i></div>
+                <div class="tileDescription">Message</div>
+            </a>
+            <a href="#" class="tile">
+                <div class="tileImage"><i class="fas fa-bullhorn fa-3x"></i></div>
+                <div class="tileDescription">Publish Notice</div>
+            </a>
+            <a href="#" class="tile">
+                <div class="tileImage"><i class="fas fa-pen fa-3x"></i></div>
+                <div class="tileDescription">Season Request Processing</div>
+            </a>
+            <a href="#" class="tile">
+                <div class="tileImage"><i class="fas fa-file-contract fa-3x"></i></div>
+                <div class="tileDescription">Add Attendance</div>
+            </a>
+            <a href="#" class="tile">
+                <div class="tileImage"><i class="fas fa-laptop-code  fa-3x"></i></div>
+                <div class="tileDescription">Add Exam Result</div>
+            </a>
+            <a href="#" class="tile">
+                <div class="tileImage"><i class="fas fa-stream  fa-3x"></i></div>
+                <div class="tileDescription">Review Exam Result</div>
+            </a>
+            <a href="#" class="tile">
+                <div class="tileImage"><i class="fas fa-handshake fa-3x"></i></div>
+                <div class="tileDescription">Get Exam Result</div>
+            </a>
+            <a href="#" class="tile">
+                <div class="tileImage"><i class="fas fa-plus-square fa-3x"></i></div>
+                <div class="tileDescription">Add IQAC Report</div>
+            </a>
+            <a href="#" class="tile">
+                <div class="tileImage"><i class="fas fa-file-upload fa-3x"></i></div>
+                <div class="tileDescription">Upload Past Papers</div>
+            </a>
+            <a href="#" class="tile">
+                <div class="tileImage"><i class="fas fa-file-csv fa-3x"></i></div>
+                <div class="tileDescription">Upload Result</div>
+            </a>
+            <a href="#" class="tile">
+                <div class="tileImage"><i class="fas fa-file-alt fa-3x"></i></div>
+                <div class="tileDescription">View IQAC Report</div>
+            </a>
+            <a href="#" class="tile">
+                <div class="tileImage"><i class="fas fa-reply fa-3x"></i></div>
+                <div class="tileDescription">Respond for Meeting Request</div>
+            </a>
+            <a href="#" class="tile">
+                <div class="tileImage"><i class="fas fa-check-double fa-3x"></i></div>
+                <div class="tileDescription">Update Availability</div>
+            </a>
+            <a href="#" class="tile">
+                <div class="tileImage"><i class="fas fa-eye fa-3x"></i></div>
+                <div class="tileDescription">Review Hall Booking Request</div>
+            </a>
+            <a href="#" class="tile">
+                <div class="tileImage"><i class="fas fa-tasks fa-3x"></i></div>
+                <div class="tileDescription">Assignment Management</div>
+            </a>
+            <a href="#" class="tile">
+                <div class="tileImage"><i class="fas fa-briefcase fa-3x"></i></div>
+                <div class="tileDescription">View Workload</div>
+            </a>
+            <a href="#" class="tile">
+                <div class="tileImage"><i class="fas fa-hand-point-right fa-3x"></i></div>
+                <div class="tileDescription">Allocate Workload</div>
+            </a>
+        </div>
     </div>
 </div>
 
