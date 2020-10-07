af92adcf211cb6a4f1b15fd37b4ed071806dd3b8
Chathura Wanniarachchi <wachathuraj@gmail.com>
2020-10-07 17:10:35 +0530

    Completely finished modification of common header and footer.

--- assets/php/commonHeader.php
+++ assets/php/commonHeader.php
@@ -1,21 +1,21 @@
 <?php
     echo("
-    <header>
-      <div class='overlay'>
-        <div class='imgSection'>
-          <img src='../../assets/images/UCSC_logo.png' alt='UCSC_logo' />
+        <header>
+            <div class='overlay'>
+                    <div class='imgSection'>
+                        <img src='../../assets/image/UOCLogo.png' alt='UOC_logo'/>
+                    </div>
+                    <div class='textSection'>
+                        <span class='mainText'>University Student-Staff Portal</span>
+                    </div>
+                    <div class='imgSection'>
+                        <img src='../../assets/image/UCSCLogo.png' alt='UCSC_logo'/>
+                    </div>
+                <h3 class='uniName'>University of Colombo School of Computing<br>Sri Lanka</h3>
+            </div>
+        </header>
+        <div class='loginInfo'>
+            <h4>Login as User &nbsp;<span><a href='#' style='color: white;'><i class='fas fa-sign-out-alt' style='color: white;'></i></a></span></h4>
         </div>
-        <div class='textSection'>
-          <span class='mainText'>University Student-Staff Portal</span>
-        </div>
-        <div class='imgSection'>
-          <img src='../../assets/images/UOC_logo.png' alt='UOC_logo' />
-        </div>
-        <h3>University of Colombo School of Computing</h3>
-      </div>
-    </header>
-    <div class='login'>
-      <h4 class='loginInfo'>Login as Chathura &nbsp;&nbsp;&nbsp;<span><a href='#' style='font-size: 80%;color: white;'>Log Out..</a></span> </h4>
-</div>
     ");
 ?>
