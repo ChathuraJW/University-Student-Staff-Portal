9afbdfad07b6f25c50af1a51e85d0a0f03c4f691
Piyumi <piyumisenarath12@gmail.com>
2020-10-07 09:32:28 +0530

    Add some styles

--- components/message/sendMessage.php
+++ components/message/sendMessage.php
@@ -1,10 +1,37 @@
-<!DOCTYPE html>
+bn<!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>University Student-Staff Portal</title>
     <link rel="stylesheet" href="../../assets/css/main.css">
+    <style>
+    .button{
+      border: none;
+      color: white;
+      padding: 6px 18px;
+      text-align: center;
+      text-decoration: none;
+      display: inline-block;
+      font-size: 16px;
+      margin: 4px 2px;
+      cursor: pointer;
+    }
+    .button1{
+      background-color: #4CAF50;
+    }
+    .button2{
+      background-color: #FF0000;
+    }
+    </style>
+
+    <style>
+      h1 {text-align: center;}
+  
+      div {text-align: center;}
+    </style>
+    
+
 </head>
 <body>
     <!-- include header section -->
@@ -12,33 +39,60 @@
     
     <!-- feature body section -->
     <div class="featureBody">
-      <hi> Send Messages </h1>
+      <hi style="font-size: 40px;"><b> Send Messages </b></h1>
+      
       <br>
-      <br>  
-
-      <label for="opt"> Enter the contacts </label>
-      <select id="opt" name="opt">
+      <br> 
+      <label for="opt"; style="font-size: 20px;"> Enter the contacts </label>
+      <br>
+      <select id="opt1" name="opt1">
       <option
         value="Academic Staff">Academic Staff</option>
       <option
-        value="Academic Supportive Staff">Academic Supportive Staff</option>
+        value="None">None</option>
+       
+      </select>
+
+      <select id="opt2" name="opt2">
+       
       <option
         value="Academic Supportive Head">Academic Supportive Head</option>
+      <option
+        value="None">None</option>
       </select>
-      <br>
-      <br>
 
-      <label> Message </label>
+      <select id="opt3" name="opt3">
+       
+      <option
+        value="Academic Supportive Staff">Academic Supportive Staff</option>
+      <option
+        value="None">None</option>
+       
+      </select>
+
       <br>
+      <br>
+      <label style="font-size: 20px;"> Title </label> 
+      <br>
+      <textarea name = "Title" cols="70"></textarea>
+      <br>
+      <br>
+      
+      
+      <label style="font-size: 20px;"> Message </label>
+      <br>
+      
       <textarea name = "Message" rows="5" cols="70"></textarea>
       <br>
 
-      <button type="button">Send
+      <button class="button button1">Send
       </button>
-      
 
-      <button type="button">Cancel
+      <button class="button button2">Cancel
       </button>
+       
+
+       
       <br>
       <br>
 
