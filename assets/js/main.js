98722c5395c7ad159d7b3b986ac3517ae0fc71ac
Chathura Wanniarachchi <wachathuraj@gmail.com>
2020-10-08 13:08:29 +0530

    Modify login page
    By  separating styles into another file and add invalid login credentials situation on JavaScript function to apply when it necessary.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,8 @@
 // login form placeholder move
-var lblUserName= document.getElementById("labelUserName");
-var lblPassword= document.getElementById("labelPassword");
-var txtUserName=document.getElementById("userName");
-var txtPassword=document.getElementById("password");
+let lblUserName = document.getElementById("labelUserName");
+let lblPassword = document.getElementById("labelPassword");
+let txtUserName = document.getElementById("userName");
+let txtPassword = document.getElementById("password");
 
 txtUserName.addEventListener("focus",function () {
     lblUserName.classList.add("small");
@@ -11,10 +11,26 @@ txtPassword.addEventListener("focus", function () {
     lblPassword.classList.add("small");
 })
 txtUserName.addEventListener("blur",function () {
-    if(txtUserName.value=='')
+    if(txtUserName.value==='')
         lblUserName.classList.remove("small");
 })
 txtPassword.addEventListener("blur",function () {
-    if(txtPassword.value=='')
+    if(txtPassword.value==='')
         lblPassword.classList.remove("small");
 })
+
+//this function for call when login errors
+function displayError(){
+    txtUserName.style.border="3px solid red";
+    txtUserName.style.backgroundColor="#efadad";
+    txtPassword.style.border="3px solid red";
+    txtPassword.style.backgroundColor="#efadad";
+    txtPassword.style.animation="shake 0.3s";
+    txtUserName.style.animation="shake 0.3s";
+    setTimeout(function(){
+        txtUserName.style.border="3px solid gray";
+        txtUserName.style.backgroundColor="#ffffff";
+        txtPassword.style.border="3px solid gray";
+        txtPassword.style.backgroundColor="#ffffff";
+    }, 3000);
+}
--- login.php
+++ login.php
@@ -4,9 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>University Student-Staff Portal</title>
-    <link rel="stylesheet" href="assets/css/main.css">
+    <link rel="stylesheet" href="assets/css/login.css">
     <style>
-        body{
+        body {
             background-repeat: no-repeat;
             background-size: cover;
             background-attachment: fixed;
@@ -16,26 +16,26 @@
     </style>
 </head>
 <body>
-    <div class="login">
-        <img src="assets/images/UNI_logo.png" alt="UNI Logo"><br>
-        <span class="title">University<br> Student-Staff<br> Portal</span><br>
-        <form action="" method="post">
-            <div class="inputPlaceholder">
-                <label for="userName" id="labelUserName">User Name</label>
-                <input type="text" name="userName" id="userName" class="inputField" autocomplete="off"><br>
-            </div>
-            <div class="inputPlaceholder">
-                <label for="password" id="labelPassword">Password</label>
-                <input type="password" name="password" id="password" class="inputField" autocomplete="off">
-            </div>
-            <br>
-            <div style="text-align: center">
-                <input type="submit" value="Log In" name="login" id="login" class="loginButton">
-            </div>
-        </form>
+<div class="login">
+    <img src="assets/images/UNI_logo.png" alt="UNI Logo"><br>
+    <span class="title">University<br> Student-Staff<br> Portal</span><br>
+    <form action="" method="post">
+        <div class="inputPlaceholder">
+            <label for="userName" id="labelUserName">User Name</label>
+            <input type="text" name="userName" id="userName" class="inputField" autocomplete="off"><br>
+        </div>
+        <div class="inputPlaceholder">
+            <label for="password" id="labelPassword">Password</label>
+            <input type="password" name="password" id="password" class="inputField" autocomplete="off">
+        </div>
         <br>
-        <a href="#"><span>Forget Password?</span></a>
-    </div>
-    <script src="assets/js/main.js"></script>
+        <div style="text-align: center">
+            <input type="submit" value="Log In" name="login" id="login" class="loginButton">
+        </div>
+    </form>
+    <br>
+    <a href="#"><span>Forget Password?</span></a>
+</div>
+<script src="assets/js/main.js"></script>
 </body>
 </html>
