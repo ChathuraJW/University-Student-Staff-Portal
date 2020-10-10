3bc741ed535e4f00d433401bb103da2e0c8edc41
Chathura Wanniarachchi <wachathuraj@gmail.com>
2020-10-10 10:00:42 +0530

    Apply grid system to addRawResult section.
    Apply homemade grid system to addRawResult feature. Apply font for all elements in this page.

--- components/exam/addRawResult.php
+++ components/exam/addRawResult.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>University Student-Staff Portal</title>
     <link rel="stylesheet" href="../../assets/css/main.css">
+    <link rel="stylesheet" href="../../assets/css/gridSystem.css">
     <link rel="stylesheet" href="resultSection.css">
 </head>
 <body>
@@ -13,83 +14,92 @@
 
 <!-- feature body section -->
 <div class="featureBody">
-    <h1 class="heading">Send Raw Result to Examination SAR</h1>
+    <div class="row col-1">
+        <div>
+            <h1 class="heading">Send Raw Result to Examination SAR</h1>
+        </div>
+    </div>
     <form action="" method="post">
-        <div class="showRadio">
-            <span>Examination for</span>
-            <div class="radioValue">
-                <input type="radio" name="examinationName" id="examination1Y" value="1y">
-                <label for="examination1Y">1<sup>st</sup> Year</label>
+        <div class="row col-3">
+            <div class="showRadio">
+                <span>Examination for</span>
+                <div class="radioValue">
+                    <input type="radio" name="examinationName" id="examination1Y" value="1y">
+                    <label for="examination1Y">1<sup>st</sup> Year</label>
+                </div>
+                <br>
+                <div class="radioValue">
+                    <input type="radio" name="examinationName" id="examination2Y" value="2y">
+                    <label for="examination2Y">2<sup>st</sup> Year</label>
+                </div>
+                <br>
+                <div class="radioValue">
+                    <input type="radio" name="examinationName" id="examination3Y" value="3y">
+                    <label for="examination3Y">3<sup>st</sup> Year</label>
+                </div>
+                <br>
+                <div class="radioValue">
+                    <input type="radio" name="examinationName" id="examination4Y" value="4y">
+                    <label for="examination4Y">4<sup>st</sup> Year</label>
+                </div>
             </div>
-            <br>
-            <div class="radioValue">
-                <input type="radio" name="examinationName" id="examination2Y" value="2y">
-                <label for="examination2Y">2<sup>st</sup> Year</label>
+            <div class="showRadio">
+                <span>Semester</span>
+                <div class="radioValue">
+                    <input type="radio" name="semester" id="semester1" value="1">
+                    <label for="semester1">1<sup>st</sup> Semester</label>
+                </div>
+                <br>
+                <div class="radioValue">
+                    <input type="radio" name="semester" id="semester2" value="2">
+                    <label for="semester2">2<sup>nd</sup> Semester</label>
+                </div>
+                <br>
             </div>
-            <br>
-            <div class="radioValue">
-                <input type="radio" name="examinationName" id="examination3Y" value="3y">
-                <label for="examination3Y">3<sup>st</sup> Year</label>
-            </div>
-            <br>
-            <div class="radioValue">
-                <input type="radio" name="examinationName" id="examination4Y" value="4y">
-                <label for="examination4Y">4<sup>st</sup> Year</label>
+            <div class="showRadio">
+                <span>Attempt</span>
+                <div class="radioValue">
+                    <input type="radio" name="attempt" id="attemptOne" value="F">
+                    <label for="attemptOne">1<sup>st</sup> Attempt</label>
+                </div>
+                <br>
+                <div class="radioValue">
+                    <input type="radio" name="attempt" id="attemptRep" value="R">
+                    <label for="attemptRep">Repeated Attempt<sup></sup></label>
+                </div>
             </div>
         </div>
-        <div class="showRadio">
-            <span>Semester</span>
-            <div class="radioValue">
-                <input type="radio" name="semester" id="semester1" value="1">
-                <label for="semester1">1<sup>st</sup> Semester</label>
+        <br>
+        <div class="row col-4">
+            <div class="showRest">
+                <span>Subject</span>
+                <select name="subject" id="subject">
+                </select>
             </div>
-            <br>
-            <div class="radioValue">
-                <input type="radio" name="semester" id="semester2" value="2">
-                <label for="semester2">2<sup>nd</sup> Semester</label>
+            <div class="showRest">
+                <span>Examination Year</span>
+                <select name="examinationYear" id="examinationYear">
+                </select>
             </div>
-            <br>
-        </div>
-        <div class="showRadio">
-            <span>Attempt</span>
-            <div class="radioValue">
-                <input type="radio" name="attempt" id="attemptOne" value="F">
-                <label for="attemptOne">1<sup>st</sup> Attempt</label>
+            <div class="showRest">
+                <span>Batch</span>
+                <select name="batch" id="batch">
+                </select>
             </div>
-            <br>
-            <div class="radioValue">
-                <input type="radio" name="attempt" id="attemptRep" value="R">
-                <label for="attemptRep">Repeated Attempt<sup></sup></label>
+            <div class="showRest">
+                <span>Result Dataset</span>
+                <label for="rawResultFile" class="fileLabel" id="rawResultFileLabel">Upload System Genarated File</label>
+                <input type="file" name="rawResultFile" id="rawResultFile"><br>
             </div>
         </div>
         <br>
-        <div class="showRest">
-            <span>Subject</span>
-            <select name="subject" id="subject">
-            </select>
-        </div>
-        <div class="showRest">
-            <span>Examination Year</span>
-            <select name="examinationYear" id="examinationYear">
-            </select>
-        </div>
-        <div class="showRest">
-            <span>Batch</span>
-            <select name="batch" id="batch">
-            </select>
-        </div>
-        <br>
-        <div class="showRest">
-            <span>USSP formatted Encrypted Result Dataset</span>
-            <label for="rawResultFile" class="fileLabel" id="rawResultFileLabel">Upload System Genarated File</label>
-            <input type="file" name="rawResultFile" id="rawResultFile"><br>
-        </div>
-        <br>
-        <div class="showRest" id="actionSection">
-            <input type="submit" value="Submit to SAR" name="submit" id="submit" class="submitCancelButton"
-                   style="background-color: rgb(0,255,0);">
-            <input type="reset" value="Cancel" name="cancel" id="cancel" class="submitCancelButton"
-                   style="background-color: rgb(255,0,0);">
+        <div class="row col-1">
+            <div class="showRest row col-2" id="actionSection">
+                <div><input type="submit" value="Submit to SAR" name="submit" id="submit" class="submitCancelButton"
+                            style="background-color: rgb(23, 193, 23);"></div>
+                <div><input type="reset" value="Cancel" name="cancel" id="cancel" class="submitCancelButton"
+                            style="background-color: rgb(255,0,0);"></div>
+            </div>
         </div>
     </form>
 </div>
