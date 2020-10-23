7abbf615edab2775319da4fb7e3105a3fce153c3
Chathura Wanniarachchi <wachathuraj@gmail.com>
2020-10-23 16:49:36 +0530

    Update with new folder structure.

--- index.php
+++ index.php
@@ -5,35 +5,17 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>University Student-Staff Portal</title>
     <link rel="stylesheet" href="assets/css/main.css">
+    <link rel="stylesheet" href="assets/css/gridSystem.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.0/css/all.min.css" />
 </head>
 <body>
-    <!-- header section -->
-    <header>
-        <div class="overlay">
-            <div class="imgSection">
-                <img src="assets/images/UOC_logo.png" alt="UOC_logo" />
-            </div>
-            <div class="textSection">
-                <span class="mainText">University Student-Staff Portal</span>
-            </div>
-            <div class="imgSection">
-                <img src="assets/images/UCSC_logo.png" alt="UCSC_logo" />
-            </div>
-            <h3>University of Colombo School of Computing</h3>
-        </div>
-    </header>
-    <div class="loginInfo">
-        <h4>Login as Chathura &nbsp;<span ><a href="#" style="color: white;"><i class="fas fa-sign-out-alt" style="color: white;"></i></a></span></h4>
-    </div>
+    <?php require_once('assets/php/basicLoader.php');?>
+    <?php BasicLoader::loadHeader('');?>
 
     <div class="content">
 
     </div>
 
-<footer class="mainFooter">
-    <br>
-    &copy; developed by Team Binary Bits
-</footer>
+    <?php BasicLoader::loadFooter('');?>
 </body>
 </html>
