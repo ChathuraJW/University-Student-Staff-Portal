169778a98b53a3ec03eecbbbd0c33fcfe2011551
Chathura Wanniarachchi <wachathuraj@gmail.com>
2020-11-01 16:21:53 +0530

    Create core structure of assignment management section.
    Basic HTML structure and CSS styling added to this section.

--- components/assignment/assets/assignmentManagement.js
+++ components/assignment/assets/assignmentManagement.js
@@ -1,6 +1,10 @@
 function addStaffRecipient(){
     let staffList=document.getElementById('academicSupportList');
     let recipientList=document.getElementById('conductStaffList');
-    recipientList.value=recipientList.value+" "+staffList.value;
+    if(recipientList.value==''){
+        recipientList.value=staffList.value;
+    }else{
+        recipientList.value=recipientList.value+"\n"+staffList.value;
+    }
     staffList.remove(staffList.selectedIndex);
 }
--- components/assignment/views/assignmentManagementView.php
+++ components/assignment/views/assignmentManagementView.php
@@ -20,13 +20,13 @@
         <div class="assignmentList">
             <span class="columnHeader">Current Assignment Planes</span>
             <?php
-                for($i=0;$i<5;$i++){
-                    echo("
+            for ($i = 0; $i < 5; $i++) {
+                echo("
                         <a href='#' class='planItem' id=''>
                             <div class='row col-2'>
                                 <div class='planInfo'>
                                     <span class='planItemHeader'>Basic Info:</span>
-                                 
+
                                     <div>
                                         <span class='dataPoint'>Data Structures and Algorithms 3</span>
                                     </div>
@@ -52,11 +52,11 @@
                             </div>
                         </a>
                     ");
-                }
+            }
             ?>
         </div>
 
-        <div class="actionSection">
+        <div class="createNewAssignmentSection">
             <form action="" method="post">
                 <span class="columnHeader">Create New Assignment Plane</span>
                 <div class="row col-2">
@@ -104,8 +104,10 @@
                 </div>
                 <div class="row col-2">
                     <div class="showRest">
-                        <span>Subject <button style="border: none;background-color: transparent;" onclick="location.reload();"><i class="fas fa-sync"></i></button></span>
+                        <span>Subject <button style="border: none;background-color: transparent;"
+                                              onclick="location.reload();"><i class="fas fa-sync"></i></button></span>
                         <select name="subject" id="subject" required>
+                            <option value="12">DSA</option>
                             <!--                        --><?php
                             //                        foreach ($controllerData as $data) {
                             //                            $year = ceil($data[semester] / 2) - 1;
@@ -127,31 +129,37 @@
                 <div class="row col-2">
                     <div class="showRest">
                         <span>Assignment Weight</span>
-                        <input type="number" name="assignmentWeight" id="assignmentWeight" max="1" min="0" step="0.01" required>
+                        <input type="number" name="assignmentWeight" id="assignmentWeight" max="1" min="0" step="0.01"
+                               required>
                     </div>
                     <div class="showRest">
                         <span>Number of Assignments </span>
-                        <input type="number" name="totalAssignment" id="totalAssignment" required>
+                        <input type="number" name="totalAssignment" id="totalAssignment" min="1" max="99" required>
                     </div>
                 </div>
                 <div class="row col-1">
                     <div class="showRest">
                         <span>Description</span><br>
-                        <textarea name="assignmentDescription" id="assignmentDescription" cols="30" rows="10"></textarea>
+                        <textarea name="assignmentDescription" id="assignmentDescription" cols="30"
+                                  rows="10"></textarea>
                     </div>
                 </div>
-                <span class="staffAddSectionHead">Assignment Plane Conducted By <button style="border: none;background-color: transparent;" onselect="location.reload();"><i class="fas fa-sync"></i></button></span>
+                <span class="staffAddSectionHead">Assignment Plane Conducted By <button
+                            style="border: none;background-color: transparent;" onselect="location.reload();"><i
+                                class="fas fa-sync"></i></button></span>
                 <div class="row col-2">
                     <div>
-                        <select name="academicSupportList" id="academicSupportList" onchange="addStaffRecipient();" required>
-                            <option value="nim">Samanthika Perera</option>
-                            <option value="abc">Saman Perera</option>
-                            <option value="mnh">Soma Gunasekare</option>
-                            <option value="qqw">Ruvan Hemarathna</option>
+                        <select name="academicSupportList" id="academicSupportList" onchange="addStaffRecipient();">
+                            <option value="">Academic Support Staff</option>
+                            <option value="Samanthika Perera-nim">Samanthika Perera</option>
+                            <option value="Saman Perera-abc">Saman Perera</option>
+                            <option value="Soma Gunasekare-mnh">Soma Gunasekare</option>
+                            <option value="Ruvan Hemarathna-qqw">Ruvan Hemarathna</option>
                         </select>
                     </div>
                     <div>
-                        <textarea name="conductStaffList" id="conductStaffList" cols="30" rows="4"></textarea>
+                        <textarea name="conductStaffList" id="conductStaffList" cols="30" rows="6" required
+                                  readonly></textarea>
                     </div>
                 </div>
                 <br>
@@ -160,6 +168,60 @@
                     <input type="reset" class="submitCancelButton red" value="Cancel">
                 </div>
             </form>
+        </div>
+        <div class="assignmentPlanManagement">
+            <span class="columnHeader">Welcome to SCS2204 Assignment Plan</span>
+            <div class="row col-2">
+                <div class="basicAssignmentInfo">
+                    <span class="sectionHeader">Basic Info:</span>
+                    <div>
+                        <span class='dataPoint'>Data Structures and Algorithms 3</span>
+                    </div>
+                    <div>
+                        <span class='dataPoint'>Subject Code: <b>SCS2201</b></span><br>
+                        <span class='dataPoint'>Degree Stream: <b>Computer Science</b></span><br>
+                        <span class='dataPoint'>Total Assignments: <b>15</b></span><br>
+                        <span class='dataPoint'>Assignment Weight: <b>40%</b></span>
+                    </div>
+                </div>
+                <div class="conductedBy">
+                    <span class="sectionHeader">Conducted By:</span>
+                    <ol class='conductBy'>
+                        <li>Mr. Ruwan Wijesingha (Owner)</li>
+                        <li>Mr. Kamal Gunaasekare</li>
+                        <li>Mrs. Samanthi Hemachandra</li>
+                        <li>Miss. Ruvani Gamage</li>
+                        <li>Miss. Ruvani Gamage</li>
+                        <li>Miss. Ruvani Gamage</li>
+                    </ol>
+                </div>
+            </div>
+            <div class="createNewAssignment">
+              <button class="submitCancelButton blue">Create New Assignment</button>
+              <button class="submitCancelButton green">Close and Complete Plane</button>
+            </div>
+            <div class="assignmentList">
+              <span class="sectionHeader">Current Assignment List:</span>
+              <div class="row col-2" style="margin:auto;">
+                <?php
+                $namArray=$arrayName = array('Lorem ipsum dolor sit.','Lorem ipsum dolor sit amet, consectetur.','Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos, neque.');
+                for ($i = 1; $i < 13; $i++) {
+                    echo("
+                        <div class='assignment'>
+                            <b><span>Assignment $i</span></b><hr>
+                            <span class='assignmentTitle'>".$namArray[$i%3]."</span>
+                            <span>Weight: 20%</span> 
+                            <div class='row col-3'>
+                                <div style='text-align: right;'><a href='#' style='color: black;'><i class='far fa-edit'></i></a></div>
+                                <div></div>
+                                <div style='text-align: left;'><a href='#' style='color: black;'><i class='far fa-folder-open'></i></a></div>
+                            </div>
+                        </div>
+                    ");
+                }
+                ?>
+              </div>
+            </div>
         </div>
     </div>
 </div>
