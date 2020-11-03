0fe2f83e2dcd40e4a5eedf8aa04f1d09860d2c6f
Chathura Wanniarachchi <wachathuraj@gmail.com>
2020-11-03 22:10:45 +0530

    Do some coding standard modifications

--- core/controllers/loginController.php
+++ core/controllers/loginController.php
@@ -1,32 +1,32 @@
 <?php
-class loginController extends Controller{
+class LoginController extends Controller{
     public static function initLogin(){
 //        create view
         self::createView("loginView");
-        if(isset($_POST['login'])){
-            $userName=$_POST['userName'];
-            $password=$_POST['password'];
-            if($password!='' && $userName!=''){
+        if (isset($_POST['login'])) {
+            $userName = $_POST['userName'];
+            $password = $_POST['password'];
+            if ($password != '' && $userName != '') {
 //                check userName validity
-                $isUserNameValid=loginModel::checkUserName($userName);
-                if($isUserNameValid){
-                    $loginStatus=loginModel::validateLogIn($userName,hash('sha256',$password));
-                    if(!$loginStatus[0]){
+                $isUserNameValid = LoginModel::checkUserName($userName);
+                if ($isUserNameValid) {
+                    $loginStatus = LoginModel::validateLogIn($userName, hash('sha256', $password));
+                    if (!$loginStatus[0]) {
 //                        display error
                         echo("<script>displayError();</script>");
-                    }else{
+                    } else {
 //                        set cookies valid for two days
-                        setcookie('userName',$userName,time()+8400*2,"/");
-                        setcookie('role',$loginStatus[0]['role'],time()+8400*2,"/");
-                        $fullName=$loginStatus[0]['firstName'].' '.$loginStatus[0]['lastName'];
-                        setcookie('fullName',$fullName,time()+8400*2,"/");
+                        setcookie('userName', $userName, time() + 8400 * 2, "/");
+                        setcookie('role', $loginStatus[0]['role'], time() + 8400 * 2, "/");
+                        $fullName = $loginStatus[0]['firstName'] . ' ' . $loginStatus[0]['lastName'];
+                        setcookie('fullName', $fullName, time() + 8400 * 2, "/");
 //                        redirect to home
                         header("Location: home");
                     }
-                }else{
+                } else {
                     echo("<script>displayError();</script>");
                 }
-            }else{
+            } else {
                 echo("<script>displayError();</script>");
             }
 //
