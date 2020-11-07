93818b744b3ea2baf2c65666448886e759d14634
Shubangi Rathnayake <shubangirathnayake97@gmail.com>
2020-11-07 17:15:59 +0530

    Get attendance data from data base to the controller

--- components/attendence/controllers/viewAttendanceController.php
+++ components/attendence/controllers/viewAttendanceController.php
@@ -4,6 +4,8 @@ class ViewAttendanceController extends Controller{
     public static function viewAttendance(){
 
         $passingSubjects=viewAttendanceModel::getSubjectData();
+        $attendanceData = viewAttendanceModel::loadAttendanceData();
+        print_r($attendanceData);
         self::createView("viewAttendanceView",$passingSubjects);
     }
 }
--- components/attendence/models/viewAttendanceModel.php
+++ components/attendence/models/viewAttendanceModel.php
@@ -1,11 +1,28 @@
 <?php
 class ViewAttendanceModel extends Model{
 
-    // public static function getSubjectName(){
-    //     $sqlQuery = "SELECT courseCode,name FROM course_module";
-    //     return  Database::executeQuery("root","",$sqlQuery);
-    // }
+    public static function loadAttendanceData(){
+        // $regNo = $_COOKIE['userName'];
+        $regNo = '2018cs136';
+        $sqlQuery = "SELECT indexNo FROM student WHERE regNo='$regNo'"; //getting index number from student table 
+        $index = Database::executeQuery("root","",$sqlQuery);
 
+        $enrollmentDetail = self::getEnrollmentID($index);
+        $attendanceSet=array();
+        foreach($enrollmentDetail as $attendanceEntry){
+            $enrollmentId = $attendanceEntry['enrollmentID'];
+            $sqlQuery = "SELECT date, week, attendance, description FROM attendance WHERE enrollmentID=$enrollmentId ORDER BY week";//how to get enrollment id
+            $attendanceDetail = Database::executeQuery("root","",$sqlQuery);
+            array_push($attendanceSet,$attendanceDetail);
+            return $attendanceSet;
+        }
+    }
+
+    public static function getEnrollmentID($studentID){
+        $sqlQuery = "SELECT enrollmentID,courseCode FROM student_enroll_course WHERE studentIndexNo=18001361 AND isActive=TRUE";
+        return Database::executeQuery("root", "", $sqlQuery);
+        
+    }
 
 
 }
