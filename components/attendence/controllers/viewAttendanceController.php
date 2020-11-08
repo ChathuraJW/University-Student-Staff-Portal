3a984a92c1ef54e82e6bf15df111c342a9d145fd
Shubangi Rathnayake <shubangirathnayake97@gmail.com>
2020-11-08 18:25:54 +0530

    Removed few files mistakenly added.

--- components/attendence/controllers/viewAttendanceController.php
+++
@@ -1,9 +0,0 @@
-<?php
-class ViewAttendanceController extends Controller{
-
-    public static function viewAttendance(){
-
-        $passingSubjects=viewAttendanceModel::getSubjectData();
-        self::createView("viewAttendanceView",$passingSubjects);
-    }
-}
