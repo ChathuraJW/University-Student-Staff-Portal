8a26e8c0008adcd41f3b47df62817bc69fb8c6fd
Rjitha Chamod <trchamod98@gmail.com>
2020-11-11 23:52:34 +0530

    Add content for search history of workload.

--- components/workLoad/views/workloadReceiveView.php
+++ components/workLoad/views/workloadReceiveView.php
@@ -17,7 +17,7 @@
     
     <!-- feature body section -->
     <div class="featureBody" >
-        <div class="row col-2" id="main"  >
+        <div class="row col-2" id="main">
 
             <div>
                 
@@ -33,11 +33,11 @@
 
             <div class="replyMessageViewer">
                 <div >
-                    <p class="messageView" id="messageView">Message Display</p>
+                    <p class="messageView" id="messageView"  >Message Display</p>
                 </div>
                 
                 <form id="workloadRequest"style="display:none;">
-                    <a class="link" href="">Check Timetable</a><br>
+                    <button class="link" onclick="scheduleCheck()">Check Schedule</button><br>
                     <h3 class="topic">Title</h3>
                     
                     <div class="displayingMessage">
@@ -82,10 +82,78 @@
                 <h3>Allocation Successful</h3>
             </div>
         </div>
+        <div class="schedule" id="schedule" style="display:none;" >
+            <h2 class='head' style="margin-bottom:20px;">My Workload</h2>
+                    
+            <?php
+                for($i=0;$i<5;$i++){
+                    echo"
+                    <div class='workloadHistory' onclick='openMsg()'>
+                        
+                        <div class='dataSet' style='float:right;'>
+                            <div class='data'>Date</div>
+                            <div class='data'>2020/2/17</div>
+                        </div>
+                        <div class='dataSet'>
+                            <div class='data'>Lecturer</div>
+                            <div class='data'>Dr Manju</div>
+                        </div>
+                        <div class='dataSet'>
+                            <div class='data'>Subject</div>
+                            <div class='data'>Subject </div>
+                        </div>
+                    </div>
+                    ";
+                }
+            ?>
+            <div class="scheduleDescription" style="display:none;">
+                <div class="scheduleMessage">
+                    <div style="padding-top:40px;">
+                        <i class="fa fa-times" style="float:right;" onclick="close()" aria-hidden="true"></i>
+                        <div class="displayingMessage">
+                            <div class="lable">Lecturer</div>
+                            <div class="value">Dr Manju</div>
+                        </div>
+                        
+                        <div class="displayingMessage">
+                            <div class="lable">Location</div>
+                            <div class="value">Hall no1</div>
+                        </div>
+                        <div class="displayingMessage">
+                            <div class="lable">Date</div>
+                            <div class="value">2020/02/17</div>
+                        </div>
+                        <div class="displayingMessage">
+                            <div class="lable">Time</div>
+                            <div class="value">3.00 PM</div>
+                        </div>
+                        <div class="displayingMessage" style="margin-bottom:10px;" >
+                            <div class="lable">Description</div>
+                            <div class="value"> Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five</div>
+                        </div>
+                        <div class="displayingMessage">
+                            <div class="lable">Reply</div>
+                            <div class="value">Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
         
     </div>
     <script>
-   
+        function openMsg(){
+            document.getElementById("scheduleDescription").style.display="";
+
+        }
+        function close(){
+            document.getElementById("scheduleDescription").style.display="none";
+        }
+        function scheduleCheck(){
+            document.getElementById("main").style.display="none";
+            // document.getElementById("messageView").style.display="none";
+            document.getElementById("schedule").style.display="";
+        }
         function openMessage(){
             document.getElementById("messageView").style.display="none";
             document.getElementById("workloadRequest").style.display="";
@@ -97,14 +165,14 @@
         }
         function allocation(){
             // document.getElementById("workloadRequest").reset();
-        document.getElementById("finalMsg").style.display="";
-        setTimeout(function(){
-            document.getElementById("workloadRequest").style.display="none";
-            document.getElementById("messageView").style.display="";
-            document.getElementById("finalMsg").style.display="none";
+            document.getElementById("finalMsg").style.display="";
+            setTimeout(function(){
+                document.getElementById("workloadRequest").style.display="none";
+                document.getElementById("messageView").style.display="";
+                document.getElementById("finalMsg").style.display="none";
 
-        }, 2000);
-    }
+                }, 2000);
+        }
     
     </script>
     
