fb1ae637cfb750414da66a39ba147b20bc447e0d
Piyumi <piyumisenarath12@gmail.com>
2020-11-12 09:47:44 +0530

    apply new assests file to trainSeason component

---
+++ components/trainSeason/controllers/checkTrainSeasonController.php
@@ -0,0 +1,35 @@
+<?php
+    class SendMessageController extends Controller{
+        public static function sendMessage(){
+            $academic=sendMessageModel::getAcademic();
+            $administrative=sendMessageModel::getAdministrative();
+            $academicSupportive=sendMessageModel::getAcademicSupportive();
+            $sendData=array($academic,$administrative,$academicSupportive);
+             
+            self::createView("sendMessageView",$sendData);
+            if(isset($_POST['submit'])){
+                $title=$_POST['title'];
+                $message=$_POST['message'];
+                $sendBy="mnj";//$_COOKIE['userName'];
+                //addd data to message table
+                $addData=sendMessageModel::addData($title,$message,$sendBy);
+                
+                $contacts=$_POST['contacts'];
+                $splitData=(explode(" ",$contacts));
+            
+                $insertData=sendMessageModel::insertData($splitData,$addData);
+
+                
+                 
+
+                  
+            }
+            
+
+             
+             
+        }
+
+    
+    }
+    ?>
---
+++ components/trainSeason/views/applyTrainSeasonView.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>University Student-Staff Portal</title>
+    <link rel="stylesheet" href="../../assets/css/main.css">
+    <link rel="stylesheet" href="../../assets/css/gridSystem.css">
+    <link rel="stylesheet" href="assets/rmStyle.css">
+</head>
+<body>
+    <!-- include header section -->
+    <?php require_once('../../assets/php/basicLoader.php') ?>
+    <?php basicLoader::loadHeader('../../'); ?>
+
+    
+    <!-- feature body section -->
+    <div class="featureBody">
+    <h1><b>Apply Train Season<b></h1>
+
+    </div>
+
+    <!-- include footer section -->
+    <?php basicLoader::loadFooter('../../'); ?>
+
+</body>
+</html>
