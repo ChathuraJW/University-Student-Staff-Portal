b06a281e5e245f366dfe4ee0e0219e68879724f6
Rjitha Chamod <trchamod98@gmail.com>
2020-11-12 14:23:52 +0530

    Create View file of timetable.

---
+++ components/timeTable/views/timetableAllocationView.php
@@ -0,0 +1,98 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>University Student-Staff Portal</title>
+    <link rel="stylesheet" href="../../assets/css/main.css">
+    <link rel="stylesheet" href="../../assets/css/workloadAllocationStyles.css">
+    <link rel="stylesheet" href="../../assets/css/gridSystem.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+<body>
+    
+    <!-- include header section -->
+    <?php require('../../assets/php/basicLoader.php')?>
+    <?php BasicLoader::loadHeader('../../') ?>
+    
+    <!-- feature body section -->
+    <div class="featureBody" >
+        <form class="row col-2" action="">
+            <div id="section" class="section">
+                <div class="row col-2">
+                    <div><button>Academic Staff</button></div>
+                    <div><button>Student</button></div>
+                </div>
+                <div>
+                    <label for="myInput">Lecturer</label>
+                    <input id="myInput" list="myDropdown" plaseholder="Search lecturer.."onkeyup="findLecture('myInput','myDropdown')">
+                    <datalist id="myDropdown">
+                        <option value="ABC1"></option>
+                        <option value="ACD2"></option>
+                        <option value="DAC3"></option>
+                        <option value="DEA4"></option>
+                        <option value="EWA5"></option>
+                    </datalist>
+                </div>
+                <div>
+                    <label for="student">students</label>
+                    <input id="student" list="studentDropdown" plaseholder="Search Students.."onkeyup="findLecture('student','studentDropdown')">
+                    <datalist id="studentDropdown">
+                        <option value="1st year CS Group1"></option>
+                        <option value="1st year CS Group2"></option>
+                        <option value="1st year IS"></option>
+                        <option value="2nd year CS Group1"></option>
+                        <option value="2nd year CS Group2"></option>
+                        <option value="3rd year CS"></option>
+                        <option value="3rd year IS"></option>
+                        <option value="4th year CS"></option>
+                        <option value="4th year IS"></option>
+
+                    </datalist>
+                    <input type="text">
+                </div>
+            </div>
+            <div>
+
+            </div>
+        </form>
+    <div>
+    <script>
+        function findLecturer(inputValue,dropdown){
+            var input, filter,div, a, i,txtValue;
+            input = document.getElementById(inputValue);
+            filter = input.value.toUpperCase();
+            div = document.getElementById(dropdown);
+            a = div.getElementsByTagName("option");
+            for (i = 0; i < a.length; i++) {
+                txtValue = a[i].textContent || a[i].innerText;
+                if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                a[i].style.display = "";
+                } else {
+                a[i].style.display = "none";
+                }
+            }
+        }
+        // function findStudents(){
+        //     var input, filter,div, a, i,txtValue;
+        //     input = document.getElementById("student");
+        //     filter = input.value.toUpperCase();
+        //     div = document.getElementById("studentDropdown");
+        //     a = div.getElementsByTagName("option");
+        //     for (i = 0; i < a.length; i++) {
+        //         txtValue = a[i].textContent || a[i].innerText;
+        //         if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        //         a[i].style.display = "";
+        //         } else {
+        //         a[i].style.display = "none";
+        //         }
+        //     }
+        // }
+    
+    </script>
+    
+
+    <!-- include footer section -->
+    <?php BasicLoader::loadFooter('../../') ?>
+</body>
+</html>
---
+++ components/timeTable/views/timetableView.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>University Student-Staff Portal</title>
+    <link rel="stylesheet" href="../../assets/css/main.css">
+    <link rel="stylesheet" href="../../assets/css/timetableStyles.css">
+    <link rel="stylesheet" href="../../assets/css/gridSystem.css">
+</head>
+<body>
+    
+    <!-- include header section -->
+    <?php require('../../assets/php/basicLoader.php')?>
+    <?php BasicLoader::loadHeader('../../') ?>
+    
+    <!-- feature body section -->
+    <div class="featureBody" >
+        <div  id="main"  >
+            <h2 class="head">Time Table</h2>
+            <table>
+                <tr>
+                    <th></th>
+                    <th>Monday</th>
+                    <th>Tuesday</th>
+                    <th>Wednesday</th>
+                    <th>Thursday</th>
+                    <th>Friday</th>
+                    <!-- <th></th> -->
+                </tr>
+                
+                <?php
+                    for($i=1;$i<18;$i++){
+
+                        if($i==1){$value="8.00-8.30";}
+                        elseif($i==2){ $value="8.30-9.00";}
+                        elseif($i==3){ $value="9.00-9.30";}
+                        elseif($i==4){$value="9.30-10.00";}
+                        elseif($i==5){$value="10.00-10.30";}
+                        elseif($i==6){$value="10.30-11.00";}
+                        elseif($i==7){$value="11.00-11.30";}
+                        elseif($i==8){$value="11.30-12.00";}
+                        elseif($i==9){$value="12.00-1.00";}
+                        elseif($i==10){$value="1.00-1.30";}
+                        elseif($i==11){$value="1.30-2.00";}
+                        elseif($i==12){$value="2.00-2.30";}
+                        elseif($i==13){$value=" 2.30-3.0";}
+                        elseif($i==14){$value="3.00-3.30";}
+                        elseif($i==15){$value="3.30-4.00";}
+                        elseif($i==16){$value="4.00-4.30";}
+                        else{$value="4.30-5.00";}
+
+                        
+                        
+                            echo "
+                                <tr class='line".$i."' id='line".$i."'>
+                                    <th >".$value."</th>
+                                    <th id='Monday".$i."' ></th>
+                                    <th id='Tuesday".$i."'></th>
+                                    <th id='Wednesday".$i."'></th>
+                                    <th id='Thursday".$i."'></th>
+                                    <th id='Friday".$i."'></th>
+                                </tr>
+                            ";
+                        
+                    }
+                ?>
+            </table>
+        </div>
+    </div>
+    <script>
+ 
+    </script>
+    
+
+    <!-- include footer section -->
+    <?php BasicLoader::loadFooter('../../') ?>
+</body>
+</html>
