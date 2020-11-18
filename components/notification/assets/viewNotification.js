ad25239b7c977293b2c0b77b2da4e72352d0c2a5
Shubangi Rathnayake <shubangirathnayake97@gmail.com>
2020-11-18 09:50:26 +0530

    Added few styles to view notification interface.

---
+++ components/notification/assets/viewNotification.js
@@ -0,0 +1,52 @@
+function displayNotification(c){
+    if(c.value=="1"){
+        document.getElementById("sortedNotification").style.visibility='visible';
+        document.getElementById("sortedNotification").style.display="";
+        document.getElementById("defaultNotification").style.visibility='hidden';
+        document.getElementById("defaultNotification").style.display="none";
+        // document.getElementById("notificationColor").style.backgroundColor = "#00FF00";
+        document.getElementById("notificationColor").classList.add('notificationNext');
+
+
+    }
+    else if(c.value=="2"){
+        document.getElementById("sortedNotification").style.visibility='visible';
+        document.getElementById("sortedNotification").style.display="";
+        document.getElementById("defaultNotification").style.visibility='hidden';
+        document.getElementById("defaultNotification").style.display="none";
+        document.getElementById("notificationColor").style.backgroundColor = "#ADD8E6";
+
+    }
+    else if(c.value=="3"){
+        document.getElementById("sortedNotification").style.visibility='visible';
+        document.getElementById("sortedNotification").style.display="";
+        document.getElementById("defaultNotification").style.visibility='hidden';
+        document.getElementById("defaultNotification").style.display="none";
+        document.getElementById("notificationColor").style.backgroundColor = "#43C6DB";
+
+    }
+    else if(c.value=="4"){
+        document.getElementById("sortedNotification").style.visibility='visible';
+        document.getElementById("sortedNotification").style.display="";
+        document.getElementById("defaultNotification").style.visibility='hidden';
+        document.getElementById("defaultNotification").style.display="none";
+        document.getElementById("notificationColor").style.backgroundColor = "#A74AC7";
+
+    }
+    else if(c.value=="5"){
+        document.getElementById("sortedNotification").style.visibility='visible';
+        document.getElementById("sortedNotification").style.display="";
+        document.getElementById("defaultNotification").style.visibility='hidden';
+        document.getElementById("defaultNotification").style.display="none";
+        document.getElementById("notificationColor").style.backgroundColor = "#4CC552";
+
+    }
+    else if(c.value=="6"){
+        document.getElementById("sortedNotification").style.visibility='visible';
+        document.getElementById("sortedNotification").style.display="";
+        document.getElementById("defaultNotification").style.visibility='hidden';
+        document.getElementById("defaultNotification").style.display="none";
+        document.getElementById("notificationColor").style.backgroundColor = "#B6B6B4";
+
+    }
+}
--- components/notification/views/viewNotificationView.php
+++ components/notification/views/viewNotificationView.php
@@ -18,39 +18,42 @@
     <div class="featureBody">
         <div class="sample row col-5">
             <div class="radioToolbar">
-                <div>
-                    <input value=1 type="radio" id="radio1" name="notificationName">
-                    <label for="radio1"><i class="fa fa-graduation-cap" aria-hidden="true"></i> Assignment, Scholarship & Lecture re-scheduling(35)</label>
+                <div class="radioStyle">
+                    <input value="6" type="radio" id="radio6" name="notificationName" onclick="displayNotification(this)">
+                    <label for="radio6"><i class="fa fa-desktop" aria-hidden="true"></i> System (35)</label><hr>
                 </div>
-                <div>
-                    <input  value=2 type="radio" id="radio2" name="notificationName">
-                    <label for="radio2"><i class="fa fa-users" aria-hidden="true"></i> Social & Events(35)</label>
+                <div class="radioStyle">
+                    <input  value="2" type="radio" id="radio2" name="notificationName" onclick="displayNotification(this)">
+                    <label for="radio2"><i class="fa fa-users" aria-hidden="true"></i> Social & Events(35)</label><hr>
                 </div>
-                <div>
-                    <input value=3 type="radio" id="radio3" name="notificationName">
-                    <label for="radio3"><i class="fa fa-bullhorn" aria-hidden="true"></i> Director Notices(35)</label>
+                <div class="radioStyle">
+                    <input value="3" type="radio" id="radio3" name="notificationName" onclick="displayNotification(this)">
+                    <label class="notificationLabel" for="radio3"><i class="fa fa-bullhorn" aria-hidden="true"></i> Director Notices(35)</label><hr>
                 </div>
-                <div>
-                    <input value=4 type="radio" id="radio4" name="notificationName">
-                    <label for="radio4"><i class="fa fa-calendar" aria-hidden="true"></i> Fundraising Events (35)</label>
+                <div class="radioStyle">
+                    <input value="4" type="radio" id="radio4" name="notificationName" onclick="displayNotification(this)">
+                    <label for="radio4"><i class="fa fa-calendar" aria-hidden="true"></i> Fundraising Events (35)</label><hr>
                 </div>
-                <div>
-                    <input value=5 type="radio" id="radio5" name="notificationName">
-                    <label for="radio4"><i class="fa fa-pencil-square" aria-hidden="true"></i> Administrative & Exam(35)</label>
+                <div class="radioStyle">
+                    <input value="5" type="radio" id="radio5" name="notificationName" onclick="displayNotification(this)">
+                    <label for="radio5"><i class="fa fa-book" aria-hidden="true"></i> Administrative & Exam(35)</label><hr>
                 </div>
-                <div>
-                    <input value=6 type="radio" id="radio6" name="notificationName">
-                    <label for="radio4">System (35)</label>
+                <div class="radioStyle">
+                    <input value="1" type="radio" id="radio1" name="notificationName" onclick="displayNotification(this)">
+                    <label for="radio1"><i class="fa fa-graduation-cap" aria-hidden="true"></i> Assignment, Scholarship & Lecture re-scheduling(35)</label><hr>
                 </div>
                 <!-- <input type="radio> -->
             </div>
-            <div class="inner row col-1">
+            <div class="inner">
+            <div class=" row col-1" >
                 <p class="heading">Notifications</p>
+            
                 <form class="example" action="action_page.php">
                     <input type="text" placeholder="Search.." name="search">
                     <button type="submit"><i class="fa fa-search"></i></button>
                 </form>
-                <?php
+                <div class="inner row col-1" id="defaultNotification">
+                    <?php
                     for($count=1;$count<=15;$count++){
                         echo("
                             <div class='notification'>
@@ -59,12 +62,28 @@
                             </div>
                         ");
                     }
-                ?>
+                    ?>
+                </div>
+                <div  class="inner row col-1" style="display:none"; id="sortedNotification">
+                    <?php
+                        for($count=1;$count<=8;$count++){
+                            echo("
+                                <div class='notification' id='notificationColor'>
+                                    <labe class='topic'><i class='fa fa-bullhorn' aria-hidden='true'></i><b> IEEEE Meeting Cancellation</b></labe>
+                                    <label class='content'>The january 12, 2021 meeting has been cancelled.The next meeting scheduled for january 21<sup>st</sup> at 8.30 a.m.</label>
+                                </div>
+                                ");
+                            }
+                    ?>
+                </div>
+                
             </div>
+                        </div>
+
         </div>
     </div>
     <!-- include footer section -->
     <?php BasicLoader::loadFooter('../../')?>
-    <script src="assets/addPastPaper.js"></script>
+    <script src="assets/viewNotification.js"></script>
 </body>
 </html>
