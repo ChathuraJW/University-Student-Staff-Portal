d997015eb50a0b0a6cf64d05cde49507f31243ae
Piyumi <piyumisenarath12@gmail.com>
2020-11-21 11:16:37 +0530

    Create and style viewIQACReport component

--- components/IQACReport/models/viewIQACModel.php
+++ components/IQACReport/models/viewIQACModel.php
@@ -1,47 +1,6 @@
 <?php
-    class RendMessageModel extends Model{
-        public static function getAcademic()
-        {
-            $sqlQuery1 = "SELECT userName,fullName FROM user WHERE role='ac'";
-            return Database::executeQuery("root","",$sqlQuery1);
-    
-        }
-
-        public static function getAdministrative()
-        {
-            $sqlQuery2 = "SELECT userName,fullName FROM user WHERE role='ad'";
-            return Database::executeQuery("root","",$sqlQuery2);
-
-        }
-
-        public static function getAcademicSupportive()
-        {
-            $sqlQuery3 = "SELECT userName,fullName FROM user WHERE role='as'";
-            return Database::executeQuery("root","",$sqlQuery3);
-        }
-
-        public static function addData($title,$message,$sendBy)
-        {
-            $insertQuery="INSERT INTO message( `title`, `message`, `timestamp`, `sendBy`) VALUES ('$title','$message',NOW(),'$sendBy')";
-            $selectQuery="SELECT messageID FROM message WHERE title='$title' AND message='$message' AND sendBy='$sendBy'";
-        
-            Database::executeQuery("root","",$insertQuery );
-            return Database::executeQuery("root","",$selectQuery )[0]['messageID'];
-        }
-        //create function to add data into messaage  table
+    class ViewIQACModel extends Model{
          
-        //return messageID
-        public static function insertData($splitData,$addData)
-        {
-            foreach($splitData as $value){
-                $insertSplitDataQuery="INSERT INTO user_receive_message(messageID,receivedBy) VALUE($addData,'$value')";
-                 
-                Database::executeQuery("root","",$insertSplitDataQuery );
-            }
-
-            
-             
-        }
     }
 
 ?>
--- components/IQACReport/views/viewIQACView.php
+++ components/IQACReport/views/viewIQACView.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>University Student-Staff Portal</title>
     <link rel="stylesheet" href="../../assets/css/main.css">
-    <link rel="stylesheet" href="assets/smStyle.css">
+    <link rel="stylesheet" href="assets/viewIQACStyle.css">
     <link rel="stylesheet" href="../../assets/css/gridSystem.css">
 
      
@@ -22,116 +22,30 @@
      
     
       <div class="featureBody">
-        <div class="container">
-          <div class="row col-1">
-         
-            <h1><b> Send Messages </b></h1>
-         
-      
-            <br>
-            <br> 
-           
-            <label for="option"> Enter the contacts </label>
-           
-          </div>
-           
-          
-          <div class="row col-3">
-            <div>
-              <select style="height:50px; width:200px; font-size:18px; background-color:white; box-shadow:8px 8px 16px 0px rgba(0,0,0,0.2);" id="academicStaffList" name="academicStaffList" onchange="addStaffRecipient('academicStaffList');">
-               
-              <option value="">Academic Staff </option>
-                <?php
-                     
-                  foreach ($controllerData[0] as $data){
-                       
-                    echo ("<option value='$data[userName]'>".$data['fullName']. " -  ".$data['userName']." </option>" );
-                  }
-                ?>
-              </select>
-              <br>
-              <br>
-            </div>
-          
-            <div>  
-              <select style="height:50px; width:200px; font-size:18px; background-color:white; box-shadow:8px 8px 16px 0px rgba(0,0,0,0.2);" id="academicSupportiveList" name="academicSupportiveList" onchange="addStaffRecipient('academicSupportiveList');"  >
-                <option value="">Academic Supportive Staff </option>
-                <?php
-                     
-                  foreach ($controllerData[1] as $data){
-                    echo ("<option value='$data[userName]'>".$data['fullName']. " -  ".$data['userName']." </option>" );
-                  }
-                ?>
-              </select>
-           
-              <br>
-              <br>
-            </div>
-
-            <div>
-              <select style="height:50px; width:200px; font-size:18px; background-color:white; box-shadow:8px 8px 16px 0px rgba(0,0,0,0.2);" id="administrativeList" name="administrativeList" onchange="addStaffRecipient('administrativeList');">
-              <option value="">Administrative Staff </option>
-                <?php
-                     
-                  foreach ($controllerData[2] as $data){
-                    echo ("<option value='$data[userName]'>".$data['fullName']. " -  ".$data['userName']." </option>" );
-                  }
-                ?>
-       
-              </select>
-           
-              <br>
-              <br>
-              
-             
-            
-            <br>
-            <br>
-          </div>
-          
-          
+         <h1><b>View IQAC Report</b></h1>
+         <div class="container">
+          <br> 
+          <select name="academicYear" id="academicYear" class="academicYear">
+            <option value="Select Academic Year">Select Academic Year</option>
+            <option value="2018/2019">2018/2019</option>
+            <option value="2017/2018">2017/2018</option>
+            <option value="2016/2017">2016/2017</option>
+          </select>
+          <br><br>
+          <select name="course" id="course" class="course">
+            <option value="Select Course Code">Select Course Code</option>
+            <option value="SCS1201">SCS1201</option>
+            <option value="SCS1202">SCS1202</option>
+            <option value="SCS1203">SCS1203</option>
+          </select>
           <br>
           <br>
-          <form action = " " method="POST">
-             
-            <label style="text-align: left">Contacts </label>
-            <br>
-            <textarea cols="100" name="contacts" id="contacts" readonly></textarea>
-           
-              <br>
-              <br>
-              <label> Title </label> 
-              <br>
-              <textarea name = "title" cols="70"></textarea>
-             
-
-             
-            <br>
-            <br>
-      
-             
-              <label> Message </label>
-              <br>
-              <textarea name = "message" rows="5" cols="70"></textarea>
-             
-
-            <br>
-
-             
-              <button class="button submit" name="submit" type="submit" style= "background-color: #4CAF50">Send
-              </button>
-
-              <button class="button cancel" colour="green" style="background-color: #FF0000">Cancel
-              </button>
-             
-
-          </form>       
-           
-          <br>
-          <br>
+          <input type="button" onclick="searchFunction()" value="Search" id="search" action="" class="search" >
+          <br><br>
+          <button class="download"  disabled="disabled" id="download" ><i class="fa fa-download"></i> Download  </button>
+           <br><br>
         </div>
-
-  
+        <br><br>
       </div>
        
 
@@ -139,7 +53,13 @@
     <!-- include footer section -->
     <?php basicLoader::loadFooter('../../'); ?>
 
-    <script src="assets/sendMessageJs.js">
+    <script>
+      function searchFunction(){
+        document.getElementById('download').removeAttribute('disabled');
+        
+      }
+
+      
     </script>
 </body>
 </html>
