62c458e4393b72fff6e1f143ce3266e3f9de4cef
Piyumi <piyumisenarath12@gmail.com>
2020-11-23 16:48:11 +0530

    Added new styles for applyTrainSeason component

--- components/trainSeason/views/applicationView.php
+++ components/trainSeason/views/applicationView.php
@@ -8,14 +8,17 @@
 </head>
 <body>
 <div class="form" id="form">
-      <form action="/action_page.php" >
-        <label for="fullName">Full Name:</label>
-        <input type="text" id="fullName" name="fullName"><br><br>
-        <label for="name">Name with initials:</label>
-        <input type="text" id="name" name="name"><br><br>
-        <label for="regNo">Registration Number:</label>
-        <input type="text" id="regNo" name="regNo"><br><br>
-        <input type="submit" value="Submit">
+      <form action="/action_page.php" class="application">
+        <label for="fullName" class="fullName">Full Name:</label>
+        <input type="text" id="fullName" name="fullName" class="fullNameText"><br><br>
+        <label for="name" class="name">Name with initials:</label>
+        <input type="text" id="name" name="name" class="nameText"><br><br>
+        <label for="regNo" class="regNo">Registration Number:</label>
+        <input type="text" id="regNo" name="regNo" class="regNoText"><br><br><br>
+        <input type="submit" value="Submit" class="submit">
+        <a class="backPage" href="applyTrainSeason"> 
+          <input type="button" value="Cancel" class="cancel">
+        </a>
       </form>
 </div>
 </body>
