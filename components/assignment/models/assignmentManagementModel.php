7fe087a5f8612ca1fc13ff489511d2e6507851cf
Chathura Wanniarachchi <wachathuraj@gmail.com>
2020-12-12 08:08:16 +0530

    Add transaction transaction management to create assignment plan section
    Add Database class for transaction management and remove Model class content due to it is useless.

--- components/assignment/models/assignmentManagementModel.php
+++ components/assignment/models/assignmentManagementModel.php
@@ -1,103 +1,115 @@
 <?php
 
-class AssignmentManagementModel extends Model{
-    public static function loadSubjectData(): array{
-        //@TODO change database credentials
-        $sqlQuery = "SELECT * FROM course_module ORDER BY semester";
-        $result = Database::executeQuery('root', '', $sqlQuery);
-        $subjectDataList = array();
-        foreach ($result as $row) {
-            $subjectDataItem = new CourseModule;
-            $subjectDataItem->createCourseModule($row['courseCode'], $row['name'], $row['creditValue'], $row['semester'], $row['description']);
-            $subjectDataList[] = $subjectDataItem;
-        }
-        return $subjectDataList;
-    }
+	class AssignmentManagementModel extends Model {
+		public static function loadSubjectData(): array {
+			//@TODO change database credentials
+			$sqlQuery = "SELECT * FROM course_module ORDER BY semester";
+			$result = Database::executeQuery('root', '', $sqlQuery);
+			$subjectDataList = array();
+			foreach ($result as $row) {
+				$subjectDataItem = new CourseModule;
+				$subjectDataItem->createCourseModule($row['courseCode'], $row['name'], $row['creditValue'], $row['semester'], $row['description']);
+				$subjectDataList[] = $subjectDataItem;
+			}
+			return $subjectDataList;
+		}
 
-    public static function loadSupportiveStaffData(): array{
-        //@TODO change database credentials
-        $sqlQuery = "SELECT * FROM user WHERE role='SP'";
-        $result = Database::executeQuery('root', '', $sqlQuery);
-        $subjectDataList = array();
-        foreach ($result as $row) {
-            $subjectDataItem = new User();
-            $subjectDataItem->createBasicUser($row['salutation'], $row['userName'], $row['firstName'], $row['lastName']);
-            $subjectDataList[] = $subjectDataItem;
-        }
-        return $subjectDataList;
-    }
+		public static function loadSupportiveStaffData(): array {
+			//@TODO change database credentials
+			$sqlQuery = "SELECT * FROM user WHERE role='SP'";
+			$result = Database::executeQuery('root', '', $sqlQuery);
+			$subjectDataList = array();
+			foreach ($result as $row) {
+				$subjectDataItem = new User();
+				$subjectDataItem->createBasicUser($row['salutation'], $row['userName'], $row['firstName'], $row['lastName']);
+				$subjectDataList[] = $subjectDataItem;
+			}
+			return $subjectDataList;
+		}
 
-    public static function addNewAssignmentPlanData($newAssignmentPlan, $conductStaffList){
-        try {
-            //@TODO change database credentials
+		public static function addNewAssignmentPlanData($newAssignmentPlan, $conductStaffList) {
+			//@TODO change database credentials
+			$database = new Database;
+			$database->establishTransaction('root', '');
 //        insert assignment plan data into assignment plan data table
-            $sqlQuery = "INSERT INTO assignment_plan (description, assignmentWeigh, totalAssignmentAmount, subject, degreeStream, isActive) VALUES ('" . $newAssignmentPlan->getDescription() . "'," . $newAssignmentPlan->getAssignmentWeight() . "," . $newAssignmentPlan->getTotalNumberOfAssignment() . ",'" . $newAssignmentPlan->getSubjectCode() . "','" . $newAssignmentPlan->getDegreeStream() . "',TRUE)";
-            $isSuccess = Database::executeQuery('root', '', $sqlQuery);
-            if ($isSuccess)
-                Model::createAudit($sqlQuery, 'assignment_plan', 'INSERT', 'Create new Assignment Plan.');
+			$sqlQuery = "INSERT INTO assignment_plan (description, assignmentWeigh, totalAssignmentAmount, subject, degreeStream, isActive) VALUES ('" . $newAssignmentPlan->getDescription() . "'," . $newAssignmentPlan->getAssignmentWeight() . "," . $newAssignmentPlan->getTotalNumberOfAssignment() . ",'" . $newAssignmentPlan->getSubjectCode() . "','" . $newAssignmentPlan->getDegreeStream() . "',TRUE)";
+			$isSuccess = $database->executeTransaction($sqlQuery);
+			if ($isSuccess)
+				$database->transactionAudit($sqlQuery, "assignment_plan", "INSERT", "create new Assignment Plan.");
 
 //        get back assignment plan id
-            //@TODO change database credentials
-            $sqlQuery = "SELECT planID FROM assignment_plan WHERE description='" . $newAssignmentPlan->getDescription() . "' AND assignmentWeigh=" . $newAssignmentPlan->getAssignmentWeight() . " AND totalAssignmentAmount=" . $newAssignmentPlan->getTotalNumberOfAssignment() . " AND subject='" . $newAssignmentPlan->getSubjectCode() . "' AND degreeStream='" . $newAssignmentPlan->getDegreeStream() . "' AND isActive=TRUE LIMIT 1";
-            $assignmentPlanID = Database::executeQuery('root', '', $sqlQuery)[0]['planID'];
+			$sqlQuery = "SELECT planID FROM assignment_plan WHERE description='" . $newAssignmentPlan->getDescription() . "' AND assignmentWeigh=" . $newAssignmentPlan->getAssignmentWeight() . " AND totalAssignmentAmount=" . $newAssignmentPlan->getTotalNumberOfAssignment() . " AND subject='" . $newAssignmentPlan->getSubjectCode() . "' AND degreeStream='" . $newAssignmentPlan->getDegreeStream() . "' AND isActive=TRUE LIMIT 1";
+			$assignmentPlanID = $database->executeTransaction($sqlQuery)[0]['planID'];
 
 //        convert staff list string into array
-            $staffList = explode("\n", $conductStaffList);
+			$staffList = explode("\n", $conductStaffList);
 //        assign each staff member to assignment plan
-            foreach ($staffList as $staff) {
-                //@TODO change database credentials
-                $sqlQuery = "INSERT INTO assignment_plan_conducted_by(staffID, assignmentPlanID) VALUES ('$staff',$assignmentPlanID)";
-                $isSuccess = Database::executeQuery('root', '', $sqlQuery);
-                if ($isSuccess)
-                    Model::createAudit($sqlQuery, 'assignment_plan_conducted_by', 'INSERT', 'Add staff member to assignment #$assignmentPlanID');
-            }
-            echo("
-                <script>alert('Assignment Plan Creation Successful.')</script>
-            ");
-        } catch (Exception $e) {
-            echo("
+			foreach ($staffList as $staff) {
+				$sqlQuery = "INSERT INTO assignment_plan_conducted_by(staffID, assignmentPlanID) VALUES ('$staff',$assignmentPlanID)";
+				$isSuccess = $database->executeTransaction($sqlQuery);
+				if ($isSuccess)
+					$database->transactionAudit($sqlQuery, "assignment_plan_conducted_by", "INSERT", "Add staff member to assignment #$assignmentPlanID");
+			}
+			if ($database->getTransactionState()) {
+//            commit transaction
+				$isCommitted = $database->commitToDatabase();
+				//@TODO Apply popup message function
+				if ($isCommitted) {
+					echo("
+                    <script>alert('Assignment Plan Creation Successful.')</script>
+                ");
+				} else {
+					echo("
                 <script>alert('Failed to Create Assignment Plan.')</script>
             ");
-        }
-    }
+				}
+			} else {
+				echo("
+                <script>alert('Failed to Create Assignment Plan.')</script>
+            ");
+			}
+//        close connection
+			$database->closeConnection();
+		}
 
-    public static function loadAssignmentPlan():array{
+		public static function loadAssignmentPlan(): array {
 //        initialization of returning array
-        $assignmentPlanArray=array();
+			$assignmentPlanArray = array();
 //        take user name of logged in user
-        $userName=$_COOKIE['userName'];
+			$userName = $_COOKIE['userName'];
 //        find active assignment plan that belongs to particular user
-        //@TODO change database credentials
-        $sqlQuery="SELECT assignmentPlanID FROM assignment_plan_conducted_by WHERE staffID='$userName'";
-        $assignmentIDs=Database::executeQuery('root','',$sqlQuery);
+			//@TODO change database credentials
+			$sqlQuery = "SELECT assignmentPlanID FROM assignment_plan_conducted_by WHERE staffID='$userName'";
+			$assignmentIDs = Database::executeQuery('root', '', $sqlQuery);
 
-        foreach ($assignmentIDs as $assignmentID){
-            //@TODO change database credentials
-            $assignmentPlanID=$assignmentID['assignmentPlanID'];
-            $sqlQuery="SELECT * FROM assignment_plan WHERE planID=$assignmentPlanID AND isActive=TRUE";
-            $result=Database::executeQuery('root','',$sqlQuery)[0];
-//            print_r($result);
+			foreach ($assignmentIDs as $assignmentID) {
+				//@TODO change database credentials
+				$assignmentPlanID = $assignmentID['assignmentPlanID'];
+				$sqlQuery = "SELECT * FROM assignment_plan WHERE planID=$assignmentPlanID AND isActive=TRUE";
+				$result = Database::executeQuery('root', '', $sqlQuery)[0];
+
 //            create object for each assignment plan
-            $newAssignmentPlan=new AssignmentPlan;
-            $newAssignmentPlan->createAssignmentPlan($result['planID'],$result['subject'],$result['degreeStream'],$result['assignmentWeigh'],$result['totalAssignmentAmount'],$result['description']);
+				$newAssignmentPlan = new AssignmentPlan;
+				$newAssignmentPlan->createAssignmentPlan($result['planID'], $result['subject'], $result['degreeStream'], $result['assignmentWeigh'], $result['totalAssignmentAmount'], $result['description']);
 //            take out staff members conduct by each assignment plan
-            //@TODO change database credentials
-            $sqlQuery="SELECT staffID FROM assignment_plan_conducted_by WHERE assignmentPlanID=$assignmentPlanID";
-            $staffList=Database::executeQuery('root','',$sqlQuery);
+
+				//@TODO change database credentials
+				$sqlQuery = "SELECT staffID FROM assignment_plan_conducted_by WHERE assignmentPlanID=$assignmentPlanID";
+				$staffList = Database::executeQuery('root', '', $sqlQuery);
 //            generate staff list and add those use object into staffArray
-            $staffArray=array();
-            foreach ($staffList as $staff){
-                $staffMember=new User;
-                $staffMember->setUserName($staff['staffID']);
-                $staffArray[]=$staffMember;
-            }
+				$staffArray = array();
+				foreach ($staffList as $staff) {
+					$staffMember = new User;
+					$staffMember->setUserName($staff['staffID']);
+					$staffArray[] = $staffMember;
+				}
 //            set staff list of assignment plan object
-            $newAssignmentPlan->setAssignmentConductBy($staffArray);
+				$newAssignmentPlan->setAssignmentConductBy($staffArray);
 
 //            finally add complete assignment plan object into returning array
-            $assignmentPlanArray[]=$newAssignmentPlan;
-        }
-        return $assignmentPlanArray;
-    }
+				$assignmentPlanArray[] = $newAssignmentPlan;
+			}
+			return $assignmentPlanArray;
+		}
 
-}
+	}
