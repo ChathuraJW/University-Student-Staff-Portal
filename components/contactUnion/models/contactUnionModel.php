2e9d7dc5e23c75c0c22d6d8f3e80496c0a0d414c
Chathura Wanniarachchi <wachathuraj@gmail.com>
2020-12-20 17:18:52 +0530

    Complete send message union section  ✔🚩
    Completed front-end ans back-end for contact union component. Add xml file call systemParameters.xml to asserts/config folder to maintain union email address. Still need to debug email function. As well as add notification toast to the component to display success or warning messages.

--- components/contactUnion/models/contactUnionModel.php
+++ components/contactUnion/models/contactUnionModel.php
@@ -1,46 +1,46 @@
 <?php
-class ContactUnionModel extends Model{
-	public static function getMessageData():array{
-		$userName=$_COOKIE['userName'];
-		$sqlQuery="SELECT * FROM send_message_to_union where sender='$userName'";
-		//@TODO change database credential
-		$result=Database::executeQuery('root','',$sqlQuery);
-//		initialize returning array
-		$returnData=array();
-		foreach ($result as $row){
-//			create individual objects and store those data for add into array
-			$contactUnionItem=new ContactUnion;
-			$contactUnionItem->setMessage($row['messageID'],$row['title'],$row['message'],$row['sender'],$row['sendTimestamp'],$row['isAnonymous']);
-//			append created object to returning array
-			$returnData[]=$contactUnionItem;
-		}
-		return $returnData;
-	}
-	public static function createMail($newUnionMessage){
-//		get client IP
-		$newUnionMessage=new ContactUnion;
-		$clientIP=$_SERVER['REMOTE_ADDR'];
-//		set timezone and get date
-		date_default_timezone_set("Asia/Colombo");
-		$date=(date("D d-m-y",time()));
 
-		if(isset($_POST['anonymousCheck']) & $_POST['anonymousCheck'] ==='on'){
-//			anonymous message
-			$sender="<b>Antonymous</b>";
-		}else{
-//        	non anonymous message
-			$sendingUser=new User;
-			$sendingUser->setUserName($newUnionMessage->getSender());
-			$sender='(<b>'.$newUnionMessage->getSender().'</b>) '.$sendingUser->getSalutation().'. '.$sendingUser->getFullName();
+	class ContactUnionModel extends Model {
+		public static function getMessageData(): array {
+			$userName = $_COOKIE['userName'];
+			$sqlQuery = "SELECT * FROM send_message_to_union where sender='$userName'";
+			$result = Database::executeQuery('student', 'student@16', $sqlQuery);
+//		initialize returning array
+			$returnData = array();
+			foreach ($result as $row) {
+//			create individual objects and store those data for add into array
+				$contactUnionItem = new ContactUnion;
+				$contactUnionItem->setMessage($row['messageID'], $row['title'], $row['message'], $row['sender'], $row['sendTimestamp'], $row['isAnonymous']);
+//			append created object to returning array
+				$returnData[] = $contactUnionItem;
+			}
+			return $returnData;
 		}
+
+		public static function createMail($newUnionMessage) {
+//		get client IP
+			$clientIP = $_SERVER['REMOTE_ADDR'];
+//		set timezone and get date
+			date_default_timezone_set("Asia/Colombo");
+			$date = (date("D d-m-y", time()));
+
+			if ($newUnionMessage->isAnonymous()) {
+//			anonymous message
+				$sender = "<b>Antonymous</b>";
+			} else {
+//        	non anonymous message
+				$sendingUser = new User;
+				$sendingUser->setUserName($newUnionMessage->getSender());
+				$sender = '(<b>' . $newUnionMessage->getSender() . '</b>) ' . $sendingUser->getSalutation() . '. ' . $sendingUser->getFullName();
+			}
 //      create email content
-		$mailContent="
+			$mailContent = "
 		            <div style='padding:5px;'>
 				        <h3 style='text-align:center;'><u>
-				        	".$newUnionMessage->getTitle()."
+				        	" . $newUnionMessage->getTitle() . "
 				         </u></h3>
 				        <p style='text-align:justify;font-size:14px;'>
-				        	".$newUnionMessage->getMessage()."
+				        	" . $newUnionMessage->getMessage() . "
 				        </p>
 				
 				        <p>
@@ -56,24 +56,45 @@ class ContactUnionModel extends Model{
 				    </div>
 		        ";
 //      start transaction
-		$dbInstance=new Database;
-		//@TODO Change DB credential
-		$dbInstance->establishTransaction('root','');
-		//@TODO Think how to handle receiver address
-		$receiver='wachathuraj@gmail.com';
-		$title="USSP System Generated Message";
-		$headers[] = 'MIME-Version: 1.0';
-		$headers[] = 'Content-type: text/html; charset=iso-8859-1';
+			$dbInstance = new Database;
+			$dbInstance->establishTransaction('student', 'student@16');
+
+//		read xml file to get union email address
+			$xmlData = simplexml_load_file("../../assets/config/systemParameters.xml");
+			$receiver = $xmlData->parameter[0]->value;
+
+//		generate main details
+			$title = "USSP System Generated Message";
+			$headers[] = 'MIME-Version: 1.0';
+			$headers[] = 'Content-type: text/html; charset=iso-8859-1';
+
 //		send the mail
-		$isSuccess=mail($receiver,$title,$mailContent,implode("\r\n", $headers));
+			//@TODO Care on mail server debugging
+			$isSuccess = mail($receiver, $title, $mailContent, implode("\r\n", $headers));
 //		insert data and log into database
-		$sqlQuery="INSERT INTO send_message_to_union(title, message, sender, sendTimestamp, isAnonymous) VALUES ('".$newUnionMessage->getTitle()."','".$newUnionMessage->getMessage()."','".$_COOKIE['userName']."',NOW(),".$newUnionMessage->isAnonymous().")";
-		$dbInstance->executeTransaction($sqlQuery);
-		$dbInstance->transactionAudit($sqlQuery,'send_message_to_union',INSERT,'Create new message and that send to the Union.');
+			$anonymousString = $newUnionMessage->isAnonymous() ? 'true' : 'false';
+			$sqlQuery = "INSERT INTO send_message_to_union(title, message, sender, sendTimestamp, isAnonymous) VALUES ('" . $newUnionMessage->getTitle() . "','" . $newUnionMessage->getMessage() . "','" . $_COOKIE['userName'] . "','NOW()','$anonymousString')";
+			$dbInstance->executeTransaction($sqlQuery);
+			$dbInstance->transactionAudit($sqlQuery, 'send_message_to_union', INSERT, 'Create new message and that send to the Union.');
+
 //		check whether all execute successfully
-		if($isSuccess && $dbInstance->getTransactionState())
-			$dbInstance->commitToDatabase();
+			if ($isSuccess && $dbInstance->getTransactionState()) {
+				$dbInstance->commitToDatabase();
+//			show success toast
+				echo("
+				<script>
+				    createToast('Success','Message Sent Successfully.','S');
+				</script> 
+			");
+			} else {
+//			show failed toast
+				echo("
+				<script>
+				    createToast('Warning','Message Sent Failed. Please Try Again.','W');
+				</script> 
+			");
+			}
 //		close db connection
-		$dbInstance->closeConnection();
+			$dbInstance->closeConnection();
+		}
 	}
-}
--- components/contactUnion/views/contactUnionView.php
+++ components/contactUnion/views/contactUnionView.php
@@ -20,42 +20,42 @@
     <div class="row col-2">
         <div class="historySection">
             <span class="columnHeader">Message History</span>
-            <?php
-	            if (sizeof($controllerData[0]) === 0)
-		            echo("
+			<?php
+				if (sizeof($controllerData[0]) === 0)
+					echo("
                         <span class='emptyMessage'>No Message History.</span>
                     ");
 
-	            for ($i = 0; $i < sizeof($controllerData); $i++) {
-		            //text size based resize button enable and content height adjustment
-		            if (strlen($controllerData[$i]->getMessage()) < 250) {
-			            $heightValue = 'auto';
-			            $displayStatus = 'none';
-		            } else {
-			            $heightValue = '110px';
-			            $displayStatus = 'block';
-		            }
-		            //anonymous check
-		            if($controllerData[$i]->isAnonymous()){
-			            $anonymousIconDisplay='inline-block';
-		            }else{
-			            $anonymousIconDisplay='none';
-		            }
+				for ($i = 0; $i < sizeof($controllerData); $i++) {
+					//text size based resize button enable and content height adjustment
+					if (strlen($controllerData[$i]->getMessage()) < 250) {
+						$heightValue = 'auto';
+						$displayStatus = 'none';
+					} else {
+						$heightValue = '110px';
+						$displayStatus = 'block';
+					}
+					//anonymous check
+					if ($controllerData[$i]->isAnonymous()) {
+						$anonymousIconDisplay = 'inline-block';
+					} else {
+						$anonymousIconDisplay = 'none';
+					}
 
-		            echo("
+					echo("
                         <div class='messageEntry normalEntry' id='entry$i'>
-                            <span class='messageEntryContent'><b>".$controllerData[$i]->getTitle()."</b></span>
+                            <span class='messageEntryContent'><b>" . $controllerData[$i]->getTitle() . "</b></span>
                             <span class='messageEntryContent' style='overflow:hidden;height:$heightValue;text-overflow: ellipsis;text-align: justify;' id='content$i'>" . $controllerData[$i]->getMessage() . "</span>
                             <span class='messageEntryContent' style='font-size:15px;'><button class='readMore' id='readMore$i' style='display:$displayStatus;' onclick='loadMore($i)'>Read More...</button></span>
-                            <span class='messageEntryContent' style='float:right;'><b><i style='display:$anonymousIconDisplay ;' class='fa fa-user-secret''></i>&nbsp;&nbsp;&nbsp;<i class='fas fa-calendar-day'>&nbsp;" . $controllerData[$i]->getData() . "</i>&nbsp;&nbsp;<i class='fas fa-clock'>&nbsp;" . $controllerData[$i]->getTime(). "</i></b></span>
+                            <span class='messageEntryContent' style='float:right;'><b><i style='display:$anonymousIconDisplay ;' class='fa fa-user-secret''></i>&nbsp;&nbsp;&nbsp;<i class='fas fa-calendar-day'>&nbsp;" . $controllerData[$i]->getData() . "</i>&nbsp;&nbsp;<i class='fas fa-clock'>&nbsp;" . $controllerData[$i]->getTime() . "</i></b></span>
                         </div>
                     ");
-	            }
-            ?>
+				}
+			?>
         </div>
         <div class="newMessageSection">
             <span class="columnHeader">Send New Message</span>
-            <form class="" method="post">
+            <form class="" method="post" id="createMessageForUnion">
                 <span class="inputHeading">Title:</span>
                 <input type="text" name="messageTitle" required><br><br>
                 <span class="inputHeading">Message:</span>
@@ -75,10 +75,10 @@
                         <i class="fa fa-user-secret fa-2x" id="anonymous" style="display: none;color: var(--dangerColor);"></i>
                     </div>
                 </div>
-                <div class="actionArea row col-2">
+                <div class="buttonCouple">
                     <input type="submit" name="sendMessage" value="Send The Message" class="button"
-                           style="margin:auto;">
-                    <input type="reset" name="" value="Cancel" class="button" style="margin:auto;">
+                           onclick="confirm('Are You Sure to Send This Message ?');">
+                    <input type="reset" name="" value="Cancel" class="button">
                 </div>
             </form>
         </div>
@@ -86,6 +86,7 @@
 </div>
 <?php basicLoader::loadFooter('../../'); ?>
 <script src="../../assets/js/jquery.js"></script>
+<script src="../../assets/js/toast.js"></script>
 <script src="assets/contactUnion.js"></script>
 </body>
 </html>
