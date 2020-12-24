abf4c2fdf9505b9135bca558f7d9fc1a4c37ffef
Shubangi Rathnayake <shubangirathnayake97@gmail.com>
2020-12-24 13:09:10 +0530

    Update index file in notification component.

--- components/notification/index.php
+++ components/notification/index.php
@@ -1,6 +1,5 @@
-<?php 
-    require_once('routes.php'); 
-    function __autoload($className){
+<?php
+    function autoload($className){
         if(file_exists("../../assets/mvc/$className.php")){
             require_once "../../assets/mvc/$className.php";
         }else if(file_exists("./controllers/$className.php")){
@@ -8,6 +7,11 @@
         }else if(file_exists("./models/$className.php")){
             require_once "./models/$className.php";
         }
+//        import class.php inside the component
+        require_once ('./assets/class.php');
+
     }
+    spl_autoload_register('autoload');
+    require_once('routes.php');
 
 ?>
