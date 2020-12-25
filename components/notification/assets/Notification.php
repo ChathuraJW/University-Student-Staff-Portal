f8428ea7f11434665bcb10046643177a9e7fb693
Shubangi Rathnayake <shubangirathnayake97@gmail.com>
2020-12-25 19:28:08 +0530

    Create a function to calculate validity period of a system notification.

--- components/notification/assets/Notification.php
+++ components/notification/assets/Notification.php
@@ -34,20 +34,32 @@ class Notification{
         // get database instance
         $databaseInstance = new Database();
         $databaseInstance->establishTransaction('root','');
-        $notificationDetailSqlQuery = "INSERT INTO notification_detail(title, content, notificationType, timestamp, validUntil, publishedByUser) VALUES ('$this->notificationTitle','$this->notificationContent',$this->notificationType,now(),$this->validWeeks,'$this->sender')";
+
+//        entre data to notification details table
+        $notificationDetailSqlQuery = "INSERT INTO notification_detail(title, content, notificationType, timestamp, validUntil, publishedByUser) 
+            VALUES ('".$this->notificationTitle."','".$this->notificationContent."',".$this->notificationType.",now(),".$this->validWeeks.",'".$this->sender."')";
         $databaseInstance->executeTransaction($notificationDetailSqlQuery);
-
         $databaseInstance->transactionAudit($notificationDetailSqlQuery,'notification_detail','INSERT', 'Publish notification Details');
-//        get notification id for just entered notification details
-        $this->notificationID = $this->getNotificationId($this->notificationTitle, $this->notificationContent, $this->notificationType, $this->validWeeks, $this->sender );
 
-        
+//        get notification id for just entered notification details
+        $this->notificationID = $this->getNotificationId();
+
+        //target audience custom
+//        user view msg nm 3
+//            calculate time out
 
     }
 
 
-    private function getNotificationId($title, $content, $notificationType, $validUntil, $publishedByUser){
-        $sqlQuery = "SELECT notificationID FROM notification_detail WHERE title='$title'AND content='$content' AND notificationType=$notificationType AND validUntil=$validUntil AND publishedByUser='$publishedByUser' ORDER BY notificationID DESC LIMIT 1";
+    private function getNotificationId(){
+        $sqlQuery = "SELECT notificationID FROM notification_detail WHERE title='".$this->notificationTitle."' AND content='".$this->notificationContent."' AND notificationType=".$this->notificationType." AND validUntil=".$this->validWeeks." AND publishedByUser='".$this->sender."' ORDER BY notificationID DESC LIMIT 1";
         return  Database::executeQuery('root','',$sqlQuery);
+    }
+
+    private function calculateValidityPeriod(){
+        $today = date('y":m:d');
+        $numberOfWeeks = $this->validWeeks;
+        $numberOfDays = $numberOfWeeks*7;
+        return date('y:m:d',strtotime('+$numberOfDays days'));
     }
 }
