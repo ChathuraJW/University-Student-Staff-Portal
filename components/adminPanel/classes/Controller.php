339b49ff6ccb40a11633ee3c20941c28c729f8ab
Chathura Wanniarachchi <wachathuraj@gmail.com>
2020-12-30 23:09:02 +0530

    Update admin panel component with latest asserts
    As well as fix the problem that are in the admin panel component, such as extra class, remove theme.

--- components/adminPanel/classes/Controller.php
+++
@@ -1,8 +0,0 @@
-<?php
-    class Controller{
-        public static function createView($viewName,$data=[]){
-            $controllerData=$data;
-            require_once("./views/$viewName.php");
-        }
-    }
-?>
