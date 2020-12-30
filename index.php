8800a629c24ba3e58d502d00d1d6c12fcadaf82c
Rjitha Chamod <trchamod98@gmail.com>
2020-12-30 15:09:52 +0530

    Change assets for Meeting appointment.

--- index.php
+++ index.php
@@ -1,43 +1,17 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>University Student-Staff Portal</title>
-    <link rel="stylesheet" href="assets/css/main.css">
-</head>
-<body>
-    <!-- header section -->
-    <div class="header">
-        <img src="assets/images/UNI_logo.png" class="headerIMG" alt="UNI Logo" height="100px" style="float: left;">
-        <img src="assets/images/USSP_logo.png" class="headerIMG" alt="USPS Logo" height="100px" style="float: right;">
-    </div>
-    <!-- log in details section -->
-    <div class="loginDetails">
-        <span class="loginData">Logged as ABS</span>
-    </div>
-    <!-- notification and log in user details -->
-    <div class="information">
-        <div class="notification">
-            <div class="card">
-            </div>
-        </div>
-        <div class="profile">
-            <div class="card">
-            </div>
-        </div>
-    </div>
-    <br>
-    <!-- feature section -->
-    <div class="featureList">
-        
-    </div>
-    <!-- footer section -->
-    <div class="footer">
-        <span class="footNote">
-        University Student-Staff Portal<br>
-        Developed by <i>Team Binary Bits</i>
-        </span>
-    </div>
-</body>
-</html>
+<?php
+    function autoload($className){
+        if(file_exists("../../assets/mvc/$className.php")){
+            require_once "../../assets/mvc/$className.php";
+        }else if(file_exists("./controllers/$className.php")){
+            require_once "./controllers/$className.php";
+        }else if(file_exists("./models/$className.php")){
+            require_once "./models/$className.php";
+        }
+//        import class.php inside the component
+        require_once ('./assets/class.php');
+
+    }
+    spl_autoload_register('autoload');
+    require_once('routes.php');
+
+?>
