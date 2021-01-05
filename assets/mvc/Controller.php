b5a0dc5bbb15d35f321f1b87f8e7203c5b6da687
Rjitha Chamod <trchamod98@gmail.com>
2021-01-05 11:17:40 +0530

    Change the Controller class.

--- assets/mvc/Controller.php
+++ assets/mvc/Controller.php
@@ -1,8 +1,13 @@
 <?php
+
     class Controller{
         public static function createView($viewName,$data=[]){
             $controllerData=$data;
             require_once("./views/$viewName.php");
+        }
+        public static function createModularView($moduleName,$viewName, $data=[]){
+	        $controllerData=$data;
+	        require_once("./views/$moduleName/$viewName.php");
         }
     }
 ?>
