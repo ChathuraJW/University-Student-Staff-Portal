02961135ea211e1c1120d912982ce0d14622937c
Shubangi Rathnayake <shubangirathnayake97@gmail.com>
2021-01-28 21:17:43 +0530

    Update new assets file and index file.

--- assets/mvc/Model.php
+++ assets/mvc/Model.php
@@ -1,21 +1,3 @@
 <?php
-class Model{
-    public static function createAudit($sqlQuery,$affectedTable,$eventType,$description){
-        $user=$_COOKIE['userName'];//take from cookies
-        $executedQuery=explode("$affectedTable",str_replace("'","",$sqlQuery))[1];
-        $auditQuery="INSERT INTO audit_log(userID, executedQuery,
-                        affectedTable, eventType, description, timestamp)
-                        VALUES ('$user','$executedQuery','$affectedTable','$eventType','$description',NOW())";
-        return Database::executeQuery("generalAccess","generalAccess@16",$auditQuery);
-    }
-
-    //subject data getting function
-    public static function getSubjectData(){
-        $sqlQuery="Select * from course_module";
-        return Database::executeQuery("student","student@16",$sqlQuery);
-    }
-    public static function getSubjectName($courseCode){
-        $sqlQuery="SELECT name FROM course_module WHERE courseCode='$courseCode' LIMIT 1";
-        return Database::executeQuery("student","student@16",$sqlQuery)[0]['name'];
-    }
-}
+	class Model {
+	}
