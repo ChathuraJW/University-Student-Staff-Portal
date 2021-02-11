79d9fd4e3ba4fdcc1e981d6e6d43bbf5bdc3d47b
Chathura Wanniarachchi <wachathuraj@gmail.com>
2021-02-12 05:15:50 +0530

    Update master with latest assets

--- assets/php/basicLoader.php
+++ assets/php/basicLoader.php
@@ -1,29 +1,32 @@
 <?php
-class BasicLoader{
-    public static function loadHeader($positionNotation){
-        echo("
+
+	class BasicLoader {
+		public static function loadHeader($positionNotation) {
+			echo("
         <header>
     <div class='overlay row col-3'>
         <div class='imgSection'>
-            <img src='{$positionNotation} assets/image/universityLogo.png' alt='UOC_logo'/>
+            <img src='{$positionNotation}assets/image/universityLogo.png' alt='UOC_logo'/>
         </div>
         <div class='textSection'>
             <span class='mainText'>University Student-Staff Portal</span><br>
             <span class='uniName'>University of Colombo School of Computing<br>Sri Lanka</span>
         </div>
         <div class='imgSection'>
-            <img src='{$positionNotation} assets/image/logoUSSP.png' alt='UCSC_logo'/>
+            <img src='{$positionNotation}assets/image/logoUSSP.png' alt='UCSC_logo'/>
         </div>
     </div>
 </header>
 <div class='loginInfo'>
-    <span class='backToHome' style='float: left;'><a href='../../' style='color: white;' title='Back to home'><i class='fas fa-home'></i></a></span>
-    <span>Login as ".$_COOKIE['fullName']." &nbsp;<span><a href='{$positionNotation} assets/php/logout.php' style='color: white;'><i class='fas fa-sign-out-alt' style='color: white;' title='Log out'></i></a></span></span>
+    <span class='backToHome' style='float: left;'><a href='../../' style='color: white;' title='Back to home'><i class='fa fa-home'></i></a></span>
+    <span>Login as " . $_COOKIE['fullName'] . " &nbsp;<span><a href='{$positionNotation}assets/php/logout.php' style='color: white;'><i class='fa fa-sign-out-alt' style='color: white;' title='Log out'></i></a></span></span>
 </div>
+<div class='toastArea' id='toastArea'></div>
     ");
-    }
-    public static function loadFooter($positionNotation){
-        echo("
+		}
+
+		public static function loadFooter($positionNotation) {
+			echo("
         <footer class='mainFooter'>
             <div class='row col-2'>
                 <div class='addressBlock'>
@@ -38,14 +41,14 @@ class BasicLoader{
                         Email: info@ucsc.cmb.ac.lk
                     </span>
                     <span>
-                        <a href='https://www.linkedin.com/school/ucsc-lk/'><i class='fab fa-linkedin fa-2x'></i></a>
-                        <a href='https://twitter.com/UCSC_LK'><i class='fab fa-twitter-square fa-2x'></i></a>
-                        <a href='https://www.facebook.com/UCSC.LK'><i class='fab fa-facebook-square fa-2x'></i></a>
-                        <a href='https://www.youtube.com/channel/UC0gdcqEL6ZZeT67s0IbOrHg'><i class='fab fa-youtube fa-2x'></i></a>
+                        <a href='https://www.linkedin.com/school/ucsc-lk/'><i class='fa fa-linkedin fa-2x'></i></a>
+                        <a href='https://twitter.com/UCSC_LK'><i class='fa fa-twitter-square fa-2x'></i></a>
+                        <a href='https://www.facebook.com/UCSC.LK'><i class='fa fa-facebook-square fa-2x'></i></a>
+                        <a href='https://www.youtube.com/channel/UC0gdcqEL6ZZeT67s0IbOrHg'><i class='fa fa-youtube fa-2x'></i></a>
                     </span>
                 </div>
                 <div class='basicDescription'>
-                        <img src='{$positionNotation} assets/image/logoUSSP.png' alt='USSPLogo'><br><br>
+                        <img src='{$positionNotation}assets/image/logoUSSP.png' alt='USSPLogo'><br><br>
                     <p>
                         University Student-Staff Portal(USSP) is developed by a second year student development team call
                         Team Binary Bits. Main purpose of this system is to automate, semi-automated university system of
@@ -60,6 +63,6 @@ class BasicLoader{
             </div>
         </footer>
     ");
-    }
-}
+		}
+	}
 
