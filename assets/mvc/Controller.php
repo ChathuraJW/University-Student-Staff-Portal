f269960e02e671d3f4792720292bd822a8f83a3f
Shubangi Rathnayake <shubangirathnayake97@gmail.com>
2021-03-25 14:32:55 +0530

    Updated attendance component with new assets file.

--- assets/mvc/Controller.php
+++ assets/mvc/Controller.php
@@ -4,5 +4,9 @@
             $controllerData=$data;
             require_once("./views/$viewName.php");
         }
+        public static function createModularView($moduleName,$viewName, $data=[]){
+	        $controllerData=$data;
+	        require_once("./views/$moduleName/$viewName.php");
+        }
     }
 ?>
