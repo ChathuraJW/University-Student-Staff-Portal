7805d031d04a653edbbbc80504da860af9b93b1a
Rjitha Chamod <trchamod98@gmail.com>
2021-03-28 10:27:27 +0530

    Update latest assets files.

--- assets/mvc/Route.php
+++ assets/mvc/Route.php
@@ -1,14 +1,16 @@
 <?php
-    class Route{
-        public static array $validRoutes =array();
 
-        public static function set($route,$function){
+	class Route {
+		public static array $validRoutes = array();
+
+		public static function set($route, $function) {
 //        	initiate timezone for server
-	        date_default_timezone_set('Asia/Colombo');
-            self::$validRoutes[]=$route;
-            if($_GET['url']==$route){
-                $function->__invoke();
-            }
-        }
-    }
+			date_default_timezone_set('Asia/Colombo');
+			self::$validRoutes[] = $route;
+			if ($_GET['url'] == $route) {
+				$function->__invoke();
+			}
+		}
+	}
+
 ?>
