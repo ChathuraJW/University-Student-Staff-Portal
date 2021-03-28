1e0ee0dfdc6d2ed5cd03b0fd7652618e33380c2c
Piyumi <piyumisenarath12@gmail.com>
2021-03-28 11:50:00 +0530

    Merge manually P-Message into P-Final

---
+++ components/message/controllers/sentBoxController.php
@@ -0,0 +1,22 @@
+<?php
+    class sentBoxController extends Controller{
+        public static function sentBox(){
+            
+                $getData = sentBoxModel::sentBoxGetMessageData();
+                print_r($getData);
+                /*$getTitle = receiveMessageModel::getTitle();
+                $getMessage = receiveMessageModel::getMessage();
+                $getSendBy = receiveMessageModel::getSendBy();*/
+                
+                 
+                
+            
+    
+                 
+                self::createView("sentBoxView",$getData);
+                 
+             
+
+
+        }
+    } 
