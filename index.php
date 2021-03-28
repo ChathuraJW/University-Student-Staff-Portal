a6002708bd5aa7f6641d2f96c16e81327b2d9b58
Chathura Wanniarachchi <wachathuraj@gmail.com>
2021-03-28 11:25:33 +0530

    Ready for merge

--- index.php
+++ index.php
@@ -1,8 +1,21 @@
-<?php
-	require_once ('assets/mvc/Database.php');
-// if cookie is set
-	if(isset($_COOKIE['userName'])){
-			header("Location: core/home");
-	}else{
-		header("Location: core/login");
-	}
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>University Student-Staff Portal</title>
+    <link rel="stylesheet" href="assets/css/main.css">
+    <link rel="stylesheet" href="assets/css/gridSystem.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.0/css/all.min.css"/>
+</head>
+<body>
+<?php require_once('assets/php/basicLoader.php'); ?>
+<?php BasicLoader::loadHeader(''); ?>
+
+<div class="content">
+
+</div>
+
+<?php BasicLoader::loadFooter(''); ?>
+</body>
+</html>
